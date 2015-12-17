4051d94a9325f0db4475744713d4c9d070c6d004
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-17 08:53:40 -0500

    Begin creating models for cards

---
+++ src/shared/constants/deck.js
@@ -0,0 +1,24 @@
+export default deck = [
+  {
+    name: "rgeezard",
+    level: 1,
+    element: null,
+    rank: {
+      left: 5,
+      top: 1,
+      right: 4,
+      bottom: 1
+    }
+  },
+  {
+    name: "rfunguar",
+    level: 1,
+    element: null,
+    rank: {
+      left: 3,
+      top: 5,
+      right: 1,
+      bottom: 1
+    }
+  },
+]
