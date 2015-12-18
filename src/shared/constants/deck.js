6b232a9a6b4a9ed153c980f41c3af210e5adfd6f
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-17 22:19:01 -0500

    Create model for level 7 cards.

--- src/shared/constants/deck.js
+++ src/shared/constants/deck.js
@@ -1,24 +1,123 @@
 export default deck = [
   {
-    name: "rgeezard",
-    level: 1,
-    element: null,
-    rank: {
-      left: 5,
-      top: 1,
-      right: 4,
-      bottom: 1
-    }
-  },
-  {
-    name: "rfunguar",
-    level: 1,
+    name: "Aeris",
+    level: 7,
     element: null,
     rank: {
       left: 3,
+      top: 8,
+      right: 6,
+      bottom: 10
+    }
+  },
+  {
+    name: "Barret",
+    level: 7,
+    element: null,
+    rank: {
+      left: 7,
+      top: 10,
+      right: 2,
+      bottom: 9
+    }
+  },
+  {
+    name: "CaitSith",
+    level: 7,
+    element: null,
+    rank: {
+      left: 10,
+      top: 8,
+      right: 5,
+      bottom: 5
+    }
+  },
+  {
+    name: "Cid",
+    level: 7,
+    element: null,
+    rank: {
+      left: 9,
       top: 5,
-      right: 1,
-      bottom: 1
+      right: 10,
+      bottom: 5
+    }
+  },
+  {
+    name: "Cloud",
+    level: 7,
+    element: null,
+    rank: {
+      left: 6,
+      top: 10,
+      right: 7,
+      bottom: 6
+    }
+  },
+  {
+    name: "Nanaki",
+    level: 7,
+    element: null,
+    rank: {
+      left: 7,
+      top: 6,
+      right: 9,
+      bottom: 7
+    }
+  },
+  {
+    name: "Rufus",
+    level: 7,
+    element: null,
+    rank: {
+      left: 10,
+      top: 5,
+      right: 6,
+      bottom: 7
+    }
+  },
+  {
+    name: "Sephiroth",
+    level: 7,
+    element: null,
+    rank: {
+      left: 8,
+      top: 8,
+      right: 10,
+      bottom: 3
+    }
+  },
+  {
+    name: "Tifa",
+    level: 7,
+    element: null,
+    rank: {
+      left: 5,
+      top: 6,
+      right: 8,
+      bottom: 10
+    }
+  },
+  {
+    name: "Vincent",
+    level: 7,
+    element: null,
+    rank: {
+      left: 10,
+      top: 1,
+      right: 9,
+      bottom: 7
+    }
+  },
+  {
+    name: "Yuffie",
+    level: 7,
+    element: null,
+    rank: {
+      left: 2,
+      top: 8,
+      right: 10,
+      bottom: 7
     }
   },
 ]
