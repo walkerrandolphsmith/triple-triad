757497b9374ce511daf7ee0586c22364c6f67db9
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-21 14:09:11 -0500

    Create a component for the round (actual game play) step

--- src/shared/components/game.jsx
+++ src/shared/components/game.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SettingsSelection from './steps/settingsSelection';
 import CardSelection from './steps/cardSelection';
+import Round from './steps/round';
 
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -19,6 +20,7 @@ export default class Game extends React.Component {
     switch(game.step){
       case 0: currentGameStep = (<SettingsSelection game={game} {...bindActionCreators(StepActions, dispatch)} />); break;
       case 1: currentGameStep = (<CardSelection game={game} {...bindActionCreators(StepActions, dispatch)} />); break;
+      case 2: currentGameStep = (<Round game={game} {...bindActionCreators(StepActions, dispatch)} />); break;
       default: console.log("default"); break;
     }
 
---
+++ src/shared/components/steps/Round.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Cards from './../cards';
+
+export default class Round extends React.Component {
+    render() {
+
+        let hand = this.props.game.hand || [];
+        let opponentHand = this.props.game.opponentHand || [];
+
+        return (
+            <div id="step-2">
+                <Cards cards={hand} />
+                <Cards cards={opponentHand} />
+            </div>
+        );
+    }
+}
