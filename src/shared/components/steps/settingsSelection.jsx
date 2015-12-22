3b59ad0d45aa9a745a443665b4cd6dd259ea99ee
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-22 02:26:57 -0500

    Format for readiablity

--- src/shared/components/steps/settingsSelection.jsx
+++ src/shared/components/steps/settingsSelection.jsx
@@ -18,9 +18,18 @@ export default class SettingsSelection extends React.Component {
     render() {
         return (
             <div id="step-0">
-                <label htmlFor="random-hand"><input type="checkbox" id="random-hand" onChange={this.randomHand}></input> Random Hand</label>
-                <label htmlFor="two-player"><input type="checkbox" id="two-player" onChange={this.multiplayer}></input>2P</label>
-                <label htmlFor="hidden-hand"><input type="checkbox" id="hidden-hand" onChange={this.visibleHand}></input>Hide opponent's hand</label>
+                <label htmlFor="random-hand">
+                    <input type="checkbox" id="random-hand" onChange={this.randomHand}></input>
+                    Random Hand
+                </label>
+                <label htmlFor="two-player">
+                    <input type="checkbox" id="two-player" onChange={this.multiplayer}></input>
+                    2P
+                </label>
+                <label htmlFor="hidden-hand">
+                    <input type="checkbox" id="hidden-hand" onChange={this.visibleHand}></input>
+                    Hide opponent's hand
+                </label>
                 <button onClick={this.props.nextStep}> Next step</button>
             </div>
         );
