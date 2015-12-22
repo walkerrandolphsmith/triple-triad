bdd57ff5c8825e6d4b475c1687e0d8c8f3ac8453
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-22 02:38:28 -0500

    a collection of cards is given a className of deck to diff.. it from the route that has an id deck

--- src/shared/components/cards.jsx
+++ src/shared/components/cards.jsx
@@ -12,7 +12,7 @@ export default class Cards extends React.Component {
         });
 
         return (
-            <div id="deck">
+            <div className="deck">
             {cardsMarkup}
             </div>
         );
