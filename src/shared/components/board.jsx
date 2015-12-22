6cd18eeaf86320fda523e987bc7aa5089637d635
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-22 02:27:12 -0500

    Create a 3 X 3 grid for the board

--- src/shared/components/board.jsx
+++ src/shared/components/board.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
+import _ from 'lodash';
 
 export default class Board extends React.Component {
-    render() {
-        return (
-            <div id="board">
 
+    click = (index) => {
+        debugger
+    };
+
+    render() {
+        let boardStyle = {
+            display: 'flex',
+            flexDirection: 'column',
+            width: '300px',
+            height: '381px',
+            margin: '0px auto'
+        };
+
+        let rowStyle = {
+            flex: '1',
+            display: 'flex'
+        };
+
+        let pieceStyle = {
+            width: '100px',
+            height: '127px',
+            fontSize: '5em',
+            flex: '1',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderTopWidth: '1px',
+            borderLeftWidth: '1px',
+            borderTopStyle: 'solid',
+            borderLeftStyle: 'solid',
+            background: 'grey'
+        };
+
+        let pieces = [];
+        for(var i = 0; i < 9; i++){
+            pieces.push(
+                <div key={i} id={i} onClick={this.click.bind(i)} style={pieceStyle}></div>
+            )
+        }
+
+        let rows = _.chunk(pieces, 3).map((group, i)=> {
+            return (<div key={i} className='row' style={rowStyle}>{group}</div>)
+        });
+
+        return (
+            <div id="board" style={boardStyle}>
+                {rows}
             </div>
         );
     }
