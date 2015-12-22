4ecc5face643e7334641465d7182943ca3c2233f
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-21 20:32:38 -0500

    Update verbiage to match the game state.

--- src/shared/components/steps/settingsSelection.jsx
+++ src/shared/components/steps/settingsSelection.jsx
@@ -20,7 +20,7 @@ export default class SettingsSelection extends React.Component {
             <div id="step-0">
                 <label htmlFor="random-hand"><input type="checkbox" id="random-hand" onChange={this.randomHand}></input> Random Hand</label>
                 <label htmlFor="two-player"><input type="checkbox" id="two-player" onChange={this.multiplayer}></input>2P</label>
-                <label htmlFor="hidden-hand"><input type="checkbox" id="hidden-hand" onChange={this.visibleHand}></input>See opponent hand</label>
+                <label htmlFor="hidden-hand"><input type="checkbox" id="hidden-hand" onChange={this.visibleHand}></input>Hide opponent's hand</label>
                 <button onClick={this.props.nextStep}> Next step</button>
             </div>
         );
