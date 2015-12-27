34e6adea97fdb6008de82549d4d7a6b608252345
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-27 00:31:40 -0500

    Unit test validPiece selector

---
+++ test/unit/selectors/validPieces-selector.spec.js
@@ -0,0 +1,32 @@
+import expect from 'expect';
+import { validPiecesSelector } from './../../../src/shared/selectors/scoreSelector';
+import deck from './../../../src/shared/constants/deck';
+import _ from 'lodash';
+
+describe("validPieces selector", () => {
+
+    describe("validPieces selector of a full board", () => {
+        it('should have no valid pieces', () => {
+            expect(validPiecesSelector({
+                board: [deck[0], deck[1], deck[2], deck[3], deck[4], deck[0], deck[1], deck[2], deck[3]]
+            })).toEqual([])
+        });
+    });
+
+    describe("validPieces selector of an empty board", () => {
+        it('should have nine valid pieces', () => {
+            expect(validPiecesSelector({
+                board: [null, null, null, null, null, null, null, null, null]
+            })).toEqual([0,1,2,3,4,5,6,7,8])
+        });
+    });
+
+    describe("validPieces selector of partially placed board", () => {
+        it('should have four valid pieces', () => {
+            expect(validPiecesSelector({
+                board: [deck[0], deck[1], null, deck[3], deck[4], deck[5], null, null, null]
+            })).toEqual([2, 6, 7, 8])
+        });
+    });
+
+});
