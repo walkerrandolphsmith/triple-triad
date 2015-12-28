68a29c3853eb3f8628f91768c56d16cb8ee5d255
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-27 21:30:38 -0500

    Test the winner selector
    - assert an in progress game has no winner
    - assert that a complete game is a tie when the scores are equal
    - assert that a complete game has a blue winner if blue score is greator than red score
    - assert that a complete game has a red winner if the blue score is less than a red score

---
+++ test/unit/selectors/winner-selector-spec.js
@@ -0,0 +1,59 @@
+import expect from 'expect';
+import { getWinner } from './../../../src/shared/selectors/winnerSelector';
+import WINNER from './../../../src/shared/constants/winner';
+import _ from 'lodash';
+
+describe("winner selector", () => {
+
+    describe("given a game in progress", () => {
+
+        let score, validPieces;
+        beforeEach(() => {
+            score = {blue: 5, red: 5};
+            validPieces = [0];
+        });
+
+        it('should be not have a winner', () => {
+            expect(getWinner(score, validPieces)).toEqual(WINNER.NONE)
+        });
+    });
+
+    describe("given a full board with equal blue and red score", () => {
+
+        let score, validPieces;
+        beforeEach(() => {
+            score = {blue: 5, red: 5};
+            validPieces = [];
+        });
+
+        it('should be not have a winner', () => {
+            expect(getWinner(score, validPieces)).toEqual(WINNER.TIE)
+        });
+    });
+
+    describe("given a full board with a blue score greater than red score", () => {
+
+        let score, validPieces;
+        beforeEach(() => {
+            score = {blue: 6, red: 4};
+            validPieces = [];
+        });
+
+        it('should be blue winner', () => {
+            expect(getWinner(score, validPieces)).toEqual(WINNER.BLUE)
+        });
+    });
+
+    describe("given a full board with a blue score less than red score", () => {
+
+        let score, validPieces;
+        beforeEach(() => {
+            score = {blue: 4, red: 6};
+            validPieces = [];
+        });
+
+        it('should be blue winner', () => {
+            expect(getWinner(score, validPieces)).toEqual(WINNER.RED)
+        });
+    });
+});
