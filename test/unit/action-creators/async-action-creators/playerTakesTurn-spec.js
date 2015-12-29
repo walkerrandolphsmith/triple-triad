3e7e2ab8f6c92a6e286f86c926f7f8591ab48d19
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-29 17:06:09 -0500

    Assert when a player (not opponent) takes a turn SELECT_PIECE action is dispatched with selected piece in the payload RULE async action dispacthed given selected piece AI_TURN asyn action dispactch
    Assert when an opponent takes a turn
    SELECT_PIECE action is dispatched with selected piece in paylaod
    RULE async action dispatched given seleceted piece

---
+++ test/unit/action-creators/async-action-creators/playerTakesTurn-spec.js
@@ -0,0 +1,61 @@
+import expect from 'expect';
+import {playerTakesTurn, rule, aiTurn} from './../../../../src/shared/action-creators/asyncActionCreators';
+
+
+describe('PLAYER_TAKES_TURN async action creator', () => {
+
+    let getState, dispatch;
+    let selectedPiece;
+    beforeEach(() => {
+        selectedPiece = 1;
+        getState = () => ({});
+        dispatch = expect.createSpy();
+    });
+
+   it('should be a function', () => {
+       expect(playerTakesTurn(selectedPiece, false)).toBeA('function');
+   });
+
+    describe('Given it is not the opponent', () => {
+
+        let isPlayer;
+        beforeEach(() => {
+            isPlayer = true;
+        });
+
+        it('should dispatch SELECT_PIECE action', () => {
+            playerTakesTurn(selectedPiece, isPlayer)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith({type: 'SelectPiece', payload: {index: selectedPiece}})
+        });
+
+        it('should dispatch RULE async action', () => {
+            playerTakesTurn(selectedPiece, isPlayer)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith(rule(selectedPiece))
+        });
+
+        it('should dispatch AI_TURN async action', () => {
+            playerTakesTurn(selectedPiece, isPlayer)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith(aiTurn())
+        });
+
+    });
+
+    describe('Given it is the opponent', () => {
+
+        let isPlayer;
+        beforeEach(() => {
+            isPlayer = false;
+        });
+
+        it('should dispatch SELECT_PIECE action', () => {
+            playerTakesTurn(selectedPiece, isPlayer)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith({type: 'SelectPiece', payload: {index: selectedPiece}})
+        });
+
+        it('should dispatch RULE async action', () => {
+            playerTakesTurn(selectedPiece, isPlayer)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith(rule(selectedPiece))
+        });
+    });
+
+});
