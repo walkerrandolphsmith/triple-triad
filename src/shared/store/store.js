f72471be0819ebb3da9838364a273425ac77ad77
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-29 12:53:14 -0500

    Update casing on import statement to avoid console warning of having two modules with same name possibly causing conflicts.. ?

--- src/shared/store/store.js
+++ src/shared/store/store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore, compose } from 'redux'
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers'
-import DevTools from './../../dev-tools/devtools';
+import DevTools from './../../dev-tools/devTools';
 
 let createStoreWithMiddleware;
 
