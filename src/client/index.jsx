5c7ec6b71b62a1cae565e71859da21de1af75976
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-29 00:46:03 -0500

    Add redux dev tools in non production environments

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Router } from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
-import routes from './../shared/routes';
-import configureStore from './../shared/store/store';
 import { Provider } from 'react-redux';
 import { fromJS } from 'immutable';
+import routes from './../shared/routes';
+import configureStore from './../shared/store/store';
+
 
 const history = createBrowserHistory();
 const store = configureStore(window.__INITIAL_STATE__);
@@ -17,3 +18,8 @@ ReactDom.render(
   </Provider>,
   mountNode
 );
+
+if (process.env.NODE_ENV !== 'production') {
+  const showDevTools = require('./../../server/showDevTools');
+  showDevTools(store);
+}
--- src/shared/store/store.js
+++ src/shared/store/store.js
@@ -1,8 +1,17 @@
-import { applyMiddleware, createStore } from 'redux'
+import { applyMiddleware, createStore, compose } from 'redux'
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers'
+import DevTools from './../../../server/devtools';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+let createStoreWithMiddleware
+if (process.env.NODE_ENV !== 'production') {
+    createStoreWithMiddleware = compose(
+        applyMiddleware(thunk),
+        DevTools.instrument()
+    )(createStore);
+}else {
+    createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+}
 
 export default function configureStore(initialState) {
   const store = createStoreWithMiddleware(rootReducer, initialState);
