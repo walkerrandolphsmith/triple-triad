1e69d024d42a0da1e3cd8493304ba3f4d87a4abd
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-30 09:25:44 -0500

    Describe permutations of same rule applied to two adjacent cards

--- test/unit/action-creators/async-action-creators/sameRule.spec.js
+++ test/unit/action-creators/async-action-creators/sameRule.spec.js
@@ -51,7 +51,7 @@ describe('SAME_RULE async action creator', () => {
         });
     });
 
-    describe('Given two adjacent cards have equal rank to your card on the sides that are touching', () => {
+    describe('Given cards to left and right have equal rank to your card on the sides that are touching', () => {
 
         let getState;
         beforeEach(() => {
@@ -72,6 +72,126 @@ describe('SAME_RULE async action creator', () => {
         it('should dispatch UPDATE_BOARD action with a payload of index of second card to flip and player owner', () => {
             sameRule(index)(dispatch, getState);
             expect(dispatch).toHaveBeenCalledWith({ type: 'UpdateBoard', payload: {index: 5, owner: player} });
+        });
+    });
+
+    describe('Given cards to top and bottom have equal rank to your card on the sides that are touching', () => {
+
+        let getState;
+        beforeEach(() => {
+            let adjacentCard = { owner: opponent, rank: { left: 5, top: 5, right: 5, bottom: 5} };
+            let adjacentCardTwo = { owner: opponent, rank: { left: 5, top: 5, right: 5, bottom: 5} };
+            getState = () => ({
+                game: {
+                    board: [null, adjacentCard, null, null, card, null, null, adjacentCardTwo, null]
+                }
+            });
+        });
+
+        it('should dispatch UPDATE_BOARD action with a payload of index of first card to flip and player owner', () => {
+            sameRule(index)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith({ type: 'UpdateBoard', payload: {index: 1, owner: player} });
+        });
+
+        it('should dispatch UPDATE_BOARD action with a payload of index of second card to flip and player owner', () => {
+            sameRule(index)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith({ type: 'UpdateBoard', payload: {index: 7, owner: player} });
+        });
+    });
+
+    describe('Given cards to left and top have equal rank to your card on the sides that are touching', () => {
+
+        let getState;
+        beforeEach(() => {
+            let adjacentCard = { owner: opponent, rank: { left: 5, top: 5, right: 5, bottom: 5} };
+            let adjacentCardTwo = { owner: opponent, rank: { left: 5, top: 5, right: 5, bottom: 5} };
+            getState = () => ({
+                game: {
+                    board: [null, adjacentCard, null, adjacentCardTwo, card, null, null, null, null]
+                }
+            });
+        });
+
+        it('should dispatch UPDATE_BOARD action with a payload of index of first card to flip and player owner', () => {
+            sameRule(index)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith({ type: 'UpdateBoard', payload: {index: 1, owner: player} });
+        });
+
+        it('should dispatch UPDATE_BOARD action with a payload of index of second card to flip and player owner', () => {
+            sameRule(index)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith({ type: 'UpdateBoard', payload: {index: 3, owner: player} });
+        });
+    });
+
+    describe('Given cards to left and bottom have equal rank to your card on the sides that are touching', () => {
+
+        let getState;
+        beforeEach(() => {
+            let adjacentCard = { owner: opponent, rank: { left: 5, top: 5, right: 5, bottom: 5} };
+            let adjacentCardTwo = { owner: opponent, rank: { left: 5, top: 5, right: 5, bottom: 5} };
+            getState = () => ({
+                game: {
+                    board: [null, adjacentCard, null, null, card, null, null, adjacentCardTwo, null]
+                }
+            });
+        });
+
+        it('should dispatch UPDATE_BOARD action with a payload of index of first card to flip and player owner', () => {
+            sameRule(index)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith({ type: 'UpdateBoard', payload: {index: 1, owner: player} });
+        });
+
+        it('should dispatch UPDATE_BOARD action with a payload of index of second card to flip and player owner', () => {
+            sameRule(index)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith({ type: 'UpdateBoard', payload: {index: 7, owner: player} });
+        });
+    });
+
+    describe('Given cards to right and top have equal rank to your card on the sides that are touching', () => {
+
+        let getState;
+        beforeEach(() => {
+            let adjacentCard = { owner: opponent, rank: { left: 5, top: 5, right: 5, bottom: 5} };
+            let adjacentCardTwo = { owner: opponent, rank: { left: 5, top: 5, right: 5, bottom: 5} };
+            getState = () => ({
+                game: {
+                    board: [null, adjacentCard, null, null, card, adjacentCardTwo, null, null, null]
+                }
+            });
+        });
+
+        it('should dispatch UPDATE_BOARD action with a payload of index of first card to flip and player owner', () => {
+            sameRule(index)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith({ type: 'UpdateBoard', payload: {index: 1, owner: player} });
+        });
+
+        it('should dispatch UPDATE_BOARD action with a payload of index of second card to flip and player owner', () => {
+            sameRule(index)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith({ type: 'UpdateBoard', payload: {index: 5, owner: player} });
+        });
+    });
+
+    describe('Given cards to right and bottom have equal rank to your card on the sides that are touching', () => {
+
+        let getState;
+        beforeEach(() => {
+            let adjacentCard = { owner: opponent, rank: { left: 5, top: 5, right: 5, bottom: 5} };
+            let adjacentCardTwo = { owner: opponent, rank: { left: 5, top: 5, right: 5, bottom: 5} };
+            getState = () => ({
+                game: {
+                    board: [null, null, null, null, card, adjacentCard, null, adjacentCardTwo, null]
+                }
+            });
+        });
+
+        it('should dispatch UPDATE_BOARD action with a payload of index of first card to flip and player owner', () => {
+            sameRule(index)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith({ type: 'UpdateBoard', payload: {index: 5, owner: player} });
+        });
+
+        it('should dispatch UPDATE_BOARD action with a payload of index of second card to flip and player owner', () => {
+            sameRule(index)(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith({ type: 'UpdateBoard', payload: {index: 7, owner: player} });
         });
     });
 });
