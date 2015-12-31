071350ad7d7e230a7c973a680ede42decddfd3e9
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-30 20:46:48 -0500

    Remove the dependecy on lodash

--- src/shared/selectors/handSelector.js
+++ src/shared/selectors/handSelector.js
@@ -1,8 +1,6 @@
-import _ from 'lodash';
-
 export const getHand = (deck, board, owner) => {
     return deck.reduce((hand, card) => {
-        if(card.owner === owner && !_.find(board, c => { return c && c.id === card.id }))
+        if(card.owner === owner && !board.find(c => { return c && c.id === card.id }))
             hand.push(card);
         return hand;
     }, [])
