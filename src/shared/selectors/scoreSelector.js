6e50c04cc92aa08c90e4fc31621125a3d2d03983
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-30 20:59:53 -0500

    Remove dependency from lodash

--- src/shared/selectors/scoreSelector.js
+++ src/shared/selectors/scoreSelector.js
@@ -1,5 +1,3 @@
-import _ from 'lodash';
-
-export const getScoreForOwner = (hand, board, owner) => { return _.compact(hand.concat(board)).reduce((x, y) => { return y.owner === owner ? x + 1 : x; }, 0)}
+export const getScoreForOwner = (hand, board, owner) => { return hand.concat(board).filter(x => x).reduce((x, y) => { return y.owner === owner ? x + 1 : x; }, 0)}
 
 export const getScore = (blue, red) => ({ blue: blue, red: red })
