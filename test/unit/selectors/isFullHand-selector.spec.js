fb5220489ddd51253928ec1fd30af41939aecdf6
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-05 01:23:55 -0500

    Update teh test for the rename

--- test/unit/selectors/isFullHand-selector.spec.js
+++ test/unit/selectors/isFullHand-selector.spec.js
@@ -1,25 +1,24 @@
 import expect from 'expect';
-import { getStepComplete } from './../../../src/shared/selectors/stepCompleteSelector';
+import { getIsFullHand } from './../../../src/shared/selectors/isFullHandSelector';
 import deck from './../../../src/shared/constants/deck';
-import _ from 'lodash';
 
 describe("stepComplete selector", () => {
 
     describe("stepComplete selector of a full hand", () => {
         it('should be true', () => {
-            expect(getStepComplete([deck[0], deck[1], deck[2], deck[3], deck[4]])).toEqual(true)
+            expect(getIsFullHand([deck[0], deck[1], deck[2], deck[3], deck[4]])).toEqual(true)
         });
     });
 
     describe("stepComplete selector of a empty hand", () => {
         it('should be false', () => {
-            expect(getStepComplete([])).toEqual(false)
+            expect(getIsFullHand([])).toEqual(false)
         });
     });
 
     describe("stepComplete selector of a partially selected hand", () => {
         it('should be false', () => {
-            expect(getStepComplete([deck[0], deck[1], deck[2]])).toEqual(false)
+            expect(getIsFullHand([deck[0], deck[1], deck[2]])).toEqual(false)
         });
     });
 
