35740200e625494960cf66b134ae546cd5d824c0
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-07 22:59:20 -0500

    Assert that the handleUp event sets the phase to pieceSelection when the phase is currently in card selection

--- test/unit/action-creators/async-action-creators/handleEnter-spec.js
+++ test/unit/action-creators/async-action-creators/handleEnter-spec.js
@@ -1,5 +1,6 @@
 import expect from 'expect';
-import { handleEnter } from './../../../../src/shared/action-creators/';
+import { Map } from 'immutable';
+import { handleEnter, setPhase } from './../../../../src/shared/action-creators/';
 
 describe('HANDLE_ENTER async action creator', () => {
 
@@ -11,6 +12,23 @@ describe('HANDLE_ENTER async action creator', () => {
 
     it('should be a function', () => {
         expect(handleEnter()).toBeA('function');
+    });
+
+    describe('given it is not the piece selection phase', () => {
+
+        let getState;
+        beforeEach(() => {
+            getState = () => ({
+                game: new Map({
+                    phase: "cardSelection"
+                })
+            });
+        });
+
+        it('should dispatch the SET_PHASE action setting the phase to pieceSelection', () => {
+            handleEnter()(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith(setPhase('pieceSelection'))
+        });
     });
 
 });
