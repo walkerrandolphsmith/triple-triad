bb79036674965c24f959e33541cd59973a785c45
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-08 09:12:28 -0500

    Added the keydown event listener on the game so it doesn't add a new listener for each round causing n keydown events

--- src/shared/containers/game.jsx
+++ src/shared/containers/game.jsx
@@ -1,7 +1,8 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import * as Actions from './../action-creators/';
 import { toJS } from 'immutable';
+import * as Actions from './../action-creators/';
+import KEY_CODE from './../constants/keyCodes';
 
 import React from 'react';
 import SettingsSelection from './settingsSelection';
@@ -10,6 +11,35 @@ import Round from './round';
 import GameOverBanner from './gameOverBanner';
 
 class Game extends React.Component {
+
+    KeyDownListener(board, event) {
+        const handleUp = board.props.handleUp;
+        const handleDown = board.props.handleDown;
+        const handleRight = board.props.handleRight;
+        const handleLeft = board.props.handleLeft;
+        const handleEnter = board.props.handleEnter;
+        const handleEscape = board.props.handleEscape;
+
+        const keyCode = event.which;
+
+        switch(keyCode){
+            case KEY_CODE.UP: handleUp(); break;
+            case KEY_CODE.DOWN: handleDown(); break;
+            case KEY_CODE.LEFT: handleLeft(); break;
+            case KEY_CODE.RIGHT: handleRight(); break;
+            case KEY_CODE.ENTER: handleEnter(); break;
+            case KEY_CODE.ESC: handleEscape(); break;
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener('keydown', this.KeyDownListener.bind(this, this));
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.KeyDownListener);
+    }
+
     render() {
         let currentGameStep = null;
 
--- src/shared/containers/round.jsx
+++ src/shared/containers/round.jsx
@@ -2,7 +2,6 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { boardSelector, handSelector, opponentHandSelector, scoreSelector, validPiecesSelector, cardSelectedSelector } from './../selectors/index';
 import * as Actions from './../action-creators/';
-import KEY_CODE from './../constants/keyCodes';
 import { toJS } from 'immutable';
 
 import React from 'react';
@@ -11,34 +10,6 @@ import Hand from './../components/hand';
 import PlayerHand from './../components/playerHand';
 
 class Round extends React.Component {
-
-    KeyDownListener(board, event) {
-        const handleUp = board.props.handleUp;
-        const handleDown = board.props.handleDown;
-        const handleRight = board.props.handleRight;
-        const handleLeft = board.props.handleLeft;
-        const handleEnter = board.props.handleEnter;
-        const handleEscape = board.props.handleEscape;
-
-        const keyCode = event.which;
-
-        switch(keyCode){
-            case KEY_CODE.UP: handleUp(); break;
-            case KEY_CODE.DOWN: handleDown(); break;
-            case KEY_CODE.LEFT: handleLeft(); break;
-            case KEY_CODE.RIGHT: handleRight(); break;
-            case KEY_CODE.ENTER: handleEnter(); break;
-            case KEY_CODE.ESC: handleEscape(); break;
-        }
-    }
-
-    componentDidMount() {
-        document.addEventListener('keydown', this.KeyDownListener.bind(this, this));
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('keydown', this.KeyDownListener);
-    }
 
     render() {
         let { game, board, hand, opponentHand, settings, score, validPieces } = this.props;
