9a67e442043f048367da11167259f128d5a3f522
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-10 20:57:42 -0500

    selectPieceByClick updates the phase to cardSelection

--- src/shared/actions/thunks/selectedPieceByClick.js
+++ src/shared/actions/thunks/selectedPieceByClick.js
@@ -1,7 +1,8 @@
+import { setPhase, selectPiece } from './../action-creators';
 import { playerTakesTurn } from './playerTakesTurn';
-import { selectPiece } from './../action-creators';
 
 export const selectedPieceByClick = (index) => (dispatch, getState) => {
     dispatch(selectPiece(index));
     dispatch(playerTakesTurn(true));
+    dispatch(setPhase('cardSelection'));
 };
