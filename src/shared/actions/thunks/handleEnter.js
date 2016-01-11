29461ebf10170e5caf15450c4d03692dc9eabaf1
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-10 20:54:20 -0500

    Handle enter action dispatches playerTakesTurn and setPhase when in pieceSelection phase

--- src/shared/actions/thunks/handleEnter.js
+++ src/shared/actions/thunks/handleEnter.js
@@ -1,13 +1,18 @@
-import { setPhase } from './../action-creators';
+import { setPhase, selectPiece } from './../action-creators';
 import { getNextSelectedPiece } from './getNextSelectedPiece';
+import { playerTakesTurn } from './playerTakesTurn';
 
 export const handleEnter = () => (dispatch, getState) => {
     const state = getState();
 
-    if(state.game.get('phase') === 'cardSelection'){
-        dispatch(setPhase('pieceSelection'));
-        dispatch(getNextSelectedPiece());
-    }else{
-
+    switch(state.game.get('phase')){
+        case 'cardSelection':
+            dispatch(setPhase('pieceSelection'));
+            dispatch(getNextSelectedPiece('enter'));
+            break;
+        case 'pieceSelection':
+            dispatch(playerTakesTurn(true));
+            dispatch(setPhase('cardSelection'));
+            break;
     }
 };
