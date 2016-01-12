f7d22d8ed93360d5fbd86a32644f20163f41caa7
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-12 18:37:00 -0500

    Routes that will become /round set the hand and getNextselectedCard

--- src/shared/actions/thunks/setHands.js
+++ src/shared/actions/thunks/setHands.js
@@ -1,14 +1,10 @@
-import { getNextSelectedCard } from './getNextSelectedCard';
 import { setHand } from './setHand';
 
 export const setHands = () => (dispatch, getState) => {
     const state = getState();
 
-    let randomHand = state.settings.get('randomHand');
-
-    if(randomHand) {
+    if(state.settings.get('randomHand'))
         dispatch(setHand(1));
-        dispatch(getNextSelectedCard());
-    }
+
     dispatch(setHand(2));
 };
--- src/shared/actions/thunks/updateRoute.js
+++ src/shared/actions/thunks/updateRoute.js
@@ -1,16 +1,17 @@
 import { pushPath } from 'redux-simple-router';
 import { setHands } from './setHands';
+import { getNextSelectedCard } from './getNextSelectedCard';
 import { newGame } from './newGame';
 
 export const updateRoute = () => (dispatch, getState) => {
     const state = getState();
 
     let randomHand = state.settings.get('randomHand');
+    //let isHandSelected = state.game.get('deck').filter(card => card.get('owner') === 1).size === 5;
 
     let nextRoute;
     switch(state.routing.path){
         case '/':
-            dispatch(setHands());
             nextRoute = randomHand ? '/round' : '/card-selection';
             break;
         case '/card-selection':
@@ -23,6 +24,10 @@ export const updateRoute = () => (dispatch, getState) => {
             dispatch(newGame());
             nextRoute = '/';
             break;
+    }
+    if(nextRoute === '/round') {
+        dispatch(setHands());
+        dispatch(getNextSelectedCard());
     }
     dispatch(pushPath(nextRoute));
 };
