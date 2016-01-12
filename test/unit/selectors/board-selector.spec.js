e7c210d73c7f85c112325b0a9c094e909a65bd40
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-11 21:26:45 -0500

    Assert Board selector retreives cards in the deck with a board index greater than -1

--- test/unit/selectors/board-selector.spec.js
+++ test/unit/selectors/board-selector.spec.js
@@ -5,23 +5,20 @@ describe("Board selector", () => {
 
     describe("given a board has cards", () => {
 
-        let deck, zero, one, two, six;
+        let deck, zero, one, two, six, cardInHandOne, cardInHandTwo;
         beforeEach(() => {
+            zero = {id: 0, name: 'Tifa', owner: 1, boardIndex: 0};
+            one = {id: 1, name: 'Tifa', owner: 1, boardIndex: 1};
+            two = {id: 2, name: 'Cloud', owner: 1, boardIndex: 2};
+            six = {id: 3, name: 'Cid', owner: 1, boardIndex: 6};
+            cardInHandOne = {id: 20, name: 'player', owner: 1, boardIndex: -1};
+            cardInHandTwo = {id: 21, name: 'opponent', owner: 2, boardIndex: -1};
 
-                zero = {id: 0, name: 'Tifa', owner: 0, boardIndex: 0};
-                one = {id: 1, name: 'Tifa', owner: 0, boardIndex: 1};
-                two = {id: 2, name: 'Cloud', owner: 0, boardIndex: 2};
-                six = {id: 3, name: 'Cid', owner: 0, boardIndex: 6};
-
-            deck = [zero, one, two, six];
-        });
-
-        it('should contain an array with nine elements', () => {
-            expect(getBoard(deck).length).toEqual(9);
+            deck = [zero, one, two, six, cardInHandOne, cardInHandTwo];
         });
 
         it('should contain an array with nulls where no cards exists and cards otherwise', () => {
-            expect(getBoard(deck)).toEqual([zero, one, two, null, null, null, six, null, null]);
+            expect(getBoard(deck)).toEqual([zero, one, two, six]);
         });
     });
 });
