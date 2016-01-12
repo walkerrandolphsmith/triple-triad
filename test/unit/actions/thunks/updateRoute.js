94dfa45b5361230ad409e803d12166919665f21f
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-12 17:40:10 -0500

    Given the root route of / dispatching update route should dispatch setHands

--- test/unit/actions/thunks/updateRoute.js
+++ test/unit/actions/thunks/updateRoute.js
@@ -5,30 +5,43 @@ import { updateRoute, __RewireAPI__ as updateRouteRewireAPI } from './../../../.
 
 describe('UPDATE_ROUTE async action creator', () => {
 
-    let getState, dispatch;
-    beforeEach(() => {
-        getState = () => ({
-            settings: new Map({
-                randomHand: false
-            }),
-            routing: {
-                path: '/'
-            }
-        });
-        dispatch = expect.createSpy();
-    });
-
     it('should be a function', () => {
         expect(updateRoute()).toBeA('function');
     });
 
-    it('should dispatch pushPath action', () => {
-        UpdateRoute.__Rewire__('pushPath', function(){
-            return 0;
+    describe('given the indexRoute of /', () => {
+        let getState, dispatch;
+        beforeEach(() => {
+            getState = () => ({
+                settings: new Map({
+                    randomHand: false
+                }),
+                routing: {
+                    path: '/'
+                }
+            });
+            dispatch = expect.createSpy();
         });
-        updateRoute()(dispatch, getState);
-        expect(dispatch).toHaveBeenCalledWith(0)
+
+        it('should dispatch pushPath action', () => {
+            UpdateRoute.__Rewire__('pushPath', function(){
+                return 0;
+            });
+            updateRoute()(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith(0)
+        });
+
+        it('should dispatch setHands action', () => {
+            UpdateRoute.__Rewire__('setHands', function(){
+                return 1;
+            });
+            updateRoute()(dispatch, getState);
+            expect(dispatch).toHaveBeenCalledWith(1)
+        });
     });
+
+
+
 
     describe('given the game is over and you play again', () => {
         let getState, dispatch;
