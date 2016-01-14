7a7fc8c9e44b695ac1fbcbd5c1bba0fd4363253f
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-13 21:51:18 -0500

    Pull the routes and child components in from routes.js

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -5,13 +5,8 @@ import { createHistory } from 'history';
 import { Provider } from 'react-redux';
 import { fromJS } from 'immutable';
 import { syncReduxAndRouter } from 'redux-simple-router'
-import routes from './../shared/routes';
+import Routes from './../shared/routes';
 import configureStore from './../shared/store/store';
-
-import Game from './../shared/containers/game';
-import SettingsSelection from './../shared/containers/settingsSelection';
-import CardSelection from './../shared/containers/cardSelection';
-import Round from './../shared/containers/round';
 
 let initialState = window.__INITIAL_STATE__;
 Object.keys(initialState).forEach(key => { initialState[key] = fromJS(initialState[key]);  });
@@ -25,11 +20,7 @@ const mountNode = document.getElementById('app');
 ReactDom.render(
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={Game}>
-        <IndexRoute component={SettingsSelection}/>
-        <Route path="card-selection" component={CardSelection}/>
-        <Route path="round" component={Round}/>
-      </Route>
+      {Routes}
     </Router>
   </Provider>,
   mountNode
