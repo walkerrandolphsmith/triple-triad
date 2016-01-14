8697355d15de1d52de37ccb2c7ef3f8d89d90919
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-14 17:31:28 -0500

    Assert that setHand dispatches ADD_CARD for each card retreived by getRandomHand

---
+++ test/unit/actions/thunks/setHand.spec.js
@@ -0,0 +1,39 @@
+import expect from 'expect';
+import { fromJS } from 'immutable';
+import SetHand from './../../../../src/shared/actions/thunks/setHand';
+import { setHand, __RewireAPI__ as setHandRewireAPI } from './../../../../src/shared/actions/thunks/setHand';
+
+describe('SET_HAND async action creator', () => {
+
+    let dispatch, getState, player, opponent;
+    beforeEach(() => {
+       player = 1;
+       opponent = 2;
+       dispatch = expect.createSpy();
+        getState = () => ({})
+    });
+
+    it('should be a function', () => {
+       expect(setHand()).toBeA('function');
+    });
+
+    it('should dispatch ADD_CARD action for each card in hand', () => {
+        SetHand.__Rewire__('getRandomHand', function(){
+            return [
+                {id: 1, name: '1', owner: 1, boardIndex: -1},
+                {id: 2, name: '2', owner: 1, boardIndex: -1},
+                {id: 3, name: '3', owner: 1, boardIndex: -1},
+                {id: 4, name: '4', owner: 1, boardIndex: -1},
+                {id: 5, name: '5', owner: 1, boardIndex: -1}
+            ];
+        });
+        SetHand.__Rewire__('addCard', function(){
+            return 1;
+        });
+
+        setHand()(dispatch, getState);
+        expect(dispatch.calls.length).toEqual(5)
+        expect(dispatch.calls[0].arguments).toEqual([1])
+    });
+
+});
