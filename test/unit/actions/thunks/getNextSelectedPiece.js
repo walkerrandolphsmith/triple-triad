c90a2bdf825fc7979abadca236686bea9700baba
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-14 17:05:11 -0500

    Assert that getNextSElectedPiece thunk collaborates with getPieceToSelect and dispatches selectPiece!

---
+++ test/unit/actions/thunks/getNextSelectedPiece.js
@@ -0,0 +1,36 @@
+import expect from 'expect';
+import { Map, List } from 'immutable';
+import GetNextSelectedPiece from './../../../../src/shared/actions/thunks/getNextSelectedPiece';
+import { getNextSelectedPiece, __RewireAPI__ as getNextSelectedPieceRewireAPI } from './../../../../src/shared/actions/thunks/getNextSelectedPiece';
+
+describe('GET_NEXT_SELECTED_PIECE async action creator', () => {
+
+    let getState, dispatch, keyCode;
+    beforeEach(() => {
+        keyCode = 13;
+        getState = () => ({
+            game: new Map({
+               deck: new List([])
+            })
+        });
+        dispatch = expect.createSpy();
+    });
+
+   it('should be a function', () => {
+       expect(getNextSelectedPiece(keyCode)).toBeA('function');
+   });
+
+    it('should dispatch the SELECT_PIECE action', () => {
+        GetNextSelectedPiece.__Rewire__('getPieceToSelect', function(){
+            return 1;
+        });
+
+        GetNextSelectedPiece.__Rewire__('selectPiece', function(){
+            return 2;
+        });
+
+        getNextSelectedPiece(keyCode)(dispatch, getState);
+        expect(dispatch).toHaveBeenCalledWith(2)
+    });
+
+});
