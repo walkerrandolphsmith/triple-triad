7a2f4f0a890e5fda3d54f0ead5ffd8528d812348
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-13 19:33:09 -0500

    Center the setting and card selection phases

--- src/shared/containers/settingsSelection.jsx
+++ src/shared/containers/settingsSelection.jsx
@@ -23,20 +23,22 @@ class SettingsSelection extends React.Component {
             <div id="settings-selection" className="container">
                 <div className="row">
                     <div className="col-md-12">
-                        <div className="control-group">
-                            <input type="checkbox" id="random-hand" onChange={this.randomHand.bind(this)}></input>
-                            <label htmlFor="random-hand"></label>
-                            <label className="text" htmlFor="random-hand">Random Hand</label>
-                        </div>
-                        <div className="control-group">
-                            <input type="checkbox" id="two-player" onChange={this.multiplayer.bind(this)}></input>
-                            <label htmlFor="two-player"></label>
-                            <label className="text" htmlFor="two-player">2 Player</label>
-                        </div>
-                        <div className="control-group">
-                            <input type="checkbox" id="hidden-hand" onChange={this.visibleHand.bind(this)}></input>
-                            <label htmlFor="hidden-hand"></label>
-                            <label className="text" htmlFor="hidden-hand">Hide opponent's hand</label>
+                        <div className="settings">
+                            <div className="control-group">
+                                <input type="checkbox" id="random-hand" onChange={this.randomHand.bind(this)}></input>
+                                <label htmlFor="random-hand"></label>
+                                <label className="text" htmlFor="random-hand">Random Hand</label>
+                            </div>
+                            <div className="control-group">
+                                <input type="checkbox" id="two-player" onChange={this.multiplayer.bind(this)}></input>
+                                <label htmlFor="two-player"></label>
+                                <label className="text" htmlFor="two-player">2 Player</label>
+                            </div>
+                            <div className="control-group">
+                                <input type="checkbox" id="hidden-hand" onChange={this.visibleHand.bind(this)}></input>
+                                <label htmlFor="hidden-hand"></label>
+                                <label className="text" htmlFor="hidden-hand">Hide opponent's hand</label>
+                            </div>
                         </div>
                         <button className="btn btn-next" onClick={this.props.updateRoute}> Next step</button>
                     </div>
