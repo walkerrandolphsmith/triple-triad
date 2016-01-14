a2b99e2f7b7c963b9024e90cac6b2aacb0ed68cf
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-14 13:04:48 -0500

    Handle enter dispatches ADD_CARD when in handSelection phase

--- src/shared/actions/thunks/handleEnter.js
+++ src/shared/actions/thunks/handleEnter.js
@@ -1,11 +1,18 @@
 import { setPhase, selectPiece } from './../action-creators';
 import { getNextSelectedPiece } from './getNextSelectedPiece';
 import { playerTakesTurn } from './playerTakesTurn';
+import { addCard } from './../action-creators';
 
 export const handleEnter = () => (dispatch, getState) => {
     const state = getState();
 
     switch(state.game.get('phase')){
+        case 'handSelection':
+            let id = state.game.get('selectedCard');
+            let card = state.game.get('deck').find(card => card.get('id') === id);
+            let owner = card.get('owner') === 1 ? 0 : 1;
+            dispatch(addCard(id, owner));
+            break;
         case 'cardSelection':
             dispatch(setPhase('pieceSelection'));
             dispatch(getNextSelectedPiece('enter'));
