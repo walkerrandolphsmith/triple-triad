952ec63893eb41a8717946fde1a987f40fd3612c
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-13 20:22:08 -0500

    Makes the settings form a controlled component, store the props of the checkboxes checked state in state and queue off of the onChange event to setState

--- src/shared/containers/settingsSelection.jsx
+++ src/shared/containers/settingsSelection.jsx
@@ -1,41 +1,60 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { toJS } from 'immutable';
 import * as Actions from './../actions/';
 
 import React from 'react';
 
 class SettingsSelection extends React.Component {
 
+    constructor(props){
+        super(props);
+
+        let { randomHand, multiplayer, visibleHand } = props.settings;
+
+        this.state = {
+            randomHand: randomHand,
+            multiplayer: multiplayer,
+            visibleHand: visibleHand
+        };
+    }
+
     randomHand(event) {
+        this.setState({randomHand: event.target.checked});
         this.props.updateSettings("randomHand", event.target.checked);
     };
 
     multiplayer(event) {
+        this.setState({multiplayer: event.target.checked});
         this.props.updateSettings("multiplayer", event.target.checked);
     };
 
     visibleHand(event) {
+        this.setState({visibleHand: event.target.checked});
         this.props.updateSettings("visibleHand", event.target.checked);
     };
 
     render() {
+
+        let { randomHand, multiplayer, visibleHand } = this.state;
+
         return (
             <div id="settings-selection" className="container">
                 <div className="row">
                     <div className="col-md-12">
                         <div className="settings">
                             <div className="control-group">
-                                <input type="checkbox" id="random-hand" onChange={this.randomHand.bind(this)}></input>
+                                <input type="checkbox" id="random-hand" checked={randomHand} onChange={this.randomHand.bind(this)}></input>
                                 <label htmlFor="random-hand"></label>
                                 <label className="text" htmlFor="random-hand">Random Hand</label>
                             </div>
                             <div className="control-group">
-                                <input type="checkbox" id="two-player" onChange={this.multiplayer.bind(this)}></input>
+                                <input type="checkbox" id="two-player" checked={multiplayer} onChange={this.multiplayer.bind(this)}></input>
                                 <label htmlFor="two-player"></label>
                                 <label className="text" htmlFor="two-player">2 Player</label>
                             </div>
                             <div className="control-group">
-                                <input type="checkbox" id="hidden-hand" onChange={this.visibleHand.bind(this)}></input>
+                                <input type="checkbox" id="hidden-hand" checked={visibleHand} onChange={this.visibleHand.bind(this)}></input>
                                 <label htmlFor="hidden-hand"></label>
                                 <label className="text" htmlFor="hidden-hand">Hide opponent's hand</label>
                             </div>
@@ -50,7 +69,7 @@ class SettingsSelection extends React.Component {
 
 function mapStateToProps(state) {
     return {
-
+        settings: state.settings.toJS()
     }
 }
 
