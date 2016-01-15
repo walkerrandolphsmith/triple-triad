11e7dc0cc6c46933cb62971a5aa8bf011285ab1c
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-15 16:28:06 -0500

    Remove action types from action creators that have since become thunks

--- src/shared/constants/action-types.js
+++ src/shared/constants/action-types.js
@@ -1,15 +1,12 @@
-export const RESET_GAME = 'ResetGame';
-export const RESET_SETTINGS = 'ResetSettings';
-export const SET_HAND = 'SetHand';
-export const SET_PHASE = 'SetPhase';
-export const ADD_CARD = 'AddCard';
 export const UPDATE_SETTINGS = 'UpdateSettings';
 export const UPDATE_FOCUS_SETTING = 'UpdateFocusSetting';
+
+export const RESET_GAME = 'ResetGame';
+export const SET_PHASE = 'SetPhase';
+export const ADD_CARD = 'AddCard';
 export const SELECT_CARD = 'SelectCard';
 export const SELECT_PIECE = 'SelectPiece';
 export const PLACE_CARD = 'PlaceCard';
 export const UPDATE_BOARD = 'UpdateBoard';
-export const APPLY_RULES = 'ApplyRules';
 export const START_AI_TURN = 'StartAITurn';
-export const AI_TURN = 'AITurn';
 export const END_AI_TURN = 'EndAITurn';
