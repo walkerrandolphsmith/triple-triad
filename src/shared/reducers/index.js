cb0ecdd8fdf4800349b527f471c138df8ca84d20
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-16 13:58:49 -0500

    combine the reducers in the reducers/index

--- src/shared/reducers/index.js
+++ src/shared/reducers/index.js
@@ -1,8 +1,11 @@
+import { combineReducers } from 'redux'
+import { routeReducer } from 'redux-simple-router';
+
 import game from './game';
 import settings from './settings';
 
-export default {
-    game, settings
-};
-
-
+export default combineReducers({
+    game: game,
+    settings: settings,
+    routing: routeReducer
+});
