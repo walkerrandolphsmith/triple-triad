b080ada2812503a9552067cab724e20e5216b78b
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-19 18:09:39 -0500

    Assert that cards owned by the player get a pointer Assert that cards not owned by player get a default cursor Assert that only the selected card get the selected class

--- test/unit/components/hand.spec.js
+++ test/unit/components/hand.spec.js
@@ -57,4 +57,139 @@ describe('DECK component', () => {
 
     });
 
+    describe('Given one card owned by the player', () => {
+        let props;
+        beforeEach(() => {
+            props = {
+                score: 10,
+                cards: [
+                    {id: 12, name: 'Cloud', owner: 1, boardIndex: -1}
+                ],
+                selectedCard: 12,
+                showBack: false
+            }
+        });
+
+        it('should render a card with a pointer', () => {
+
+            class Card extends React.Component {
+                render() {
+                    return (<div></div>)
+                }
+            }
+            Hand.__Rewire__('Card', Card);
+
+
+            renderer.render(
+                <Hand score={props.score} cards={props.cards} selectedCard={props.selectedCard} showBack={props.showBack} />
+            );
+            const actualElement = renderer.getRenderOutput();
+            const expectedElement =
+                <div className="hand">
+                    <div className="score">{props.score}</div>
+                    <Card card={props.cards[0]}
+                        cardStyle={{cursor: 'pointer'}}
+                        classes="selected"
+                        clickAction={() => {}}>
+                    </Card>
+                </div>
+
+            expect(actualElement).toEqualJSX(expectedElement);
+        })
+
+    });
+
+    describe('Given one card not owned by the player', () => {
+        let props;
+        beforeEach(() => {
+            props = {
+                score: 10,
+                cards: [
+                    {id: 12, name: 'Cloud', owner: 2, boardIndex: -1}
+                ],
+                selectedCard: 12,
+                showBack: false
+            }
+        });
+
+        it('should render a card with a default cursor', () => {
+
+            class Card extends React.Component {
+                render() {
+                    return (<div></div>)
+                }
+            }
+            Hand.__Rewire__('Card', Card);
+
+
+            renderer.render(
+                <Hand score={props.score} cards={props.cards} selectedCard={props.selectedCard} showBack={props.showBack} />
+            );
+            const actualElement = renderer.getRenderOutput();
+            const expectedElement =
+                <div className="hand">
+                    <div className="score">{props.score}</div>
+                    <Card card={props.cards[0]}
+                        cardStyle={{cursor: 'default'}}
+                        classes="selected"
+                        clickAction={() => {}}>
+                    </Card>
+                </div>
+
+            expect(actualElement).toEqualJSX(expectedElement);
+        })
+
+    });
+
+    describe('Given many cards in which only one is selected', () => {
+        let props;
+        beforeEach(() => {
+            props = {
+                score: 10,
+                cards: [
+                    {id: 12, name: 'Cloud', owner: 2, boardIndex: -1},
+                    {id: 2, name: 'Cloud', owner: 2, boardIndex: -1}
+                ],
+                selectedCard: 12,
+                showBack: false
+            }
+        });
+
+        it('should render a card with selected class and others without a class', () => {
+
+            class Card extends React.Component {
+                render() {
+                    return (<div></div>)
+                }
+            }
+            Hand.__Rewire__('Card', Card);
+
+
+            renderer.render(
+                <Hand score={props.score} cards={props.cards} selectedCard={props.selectedCard} showBack={props.showBack} />
+            );
+            const actualElement = renderer.getRenderOutput();
+            const expectedElement =
+                <div className="hand">
+                    <div className="score">{props.score}</div>
+                    <Card card={props.cards[0]}
+                        cardStyle={{cursor: 'default'}}
+                        classes="selected"
+                        clickAction={() => {}}>
+                    </Card>
+                    <Card card={props.cards[1]}
+                        cardStyle={{cursor: 'default'}}
+                        classes=""
+                        clickAction={() => {}}>
+                    </Card>
+                </div>
+
+            expect(actualElement).toEqualJSX(expectedElement);
+        })
+
+    });
+
+
+
+
 });
