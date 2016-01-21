fe4c07604521ac99b97b48f5305fc18dad3e6866
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-20 22:30:51 -0500

    Remove console logs

--- src/shared/actions/utils/getValidPiece.js
+++ src/shared/actions/utils/getValidPiece.js
@@ -4,8 +4,6 @@ import { getValidPieces } from './../../selectors/validPiecesSelector';
 
 export function getValidPiece(game){
     const board = getBoard(game.get('deck'));
-    console.log(board);
     let validPieces = getValidPieces(board);
-    console.log(validPieces);
     return validPieces.size > 0 ? sample(validPieces) : -1;
 }
