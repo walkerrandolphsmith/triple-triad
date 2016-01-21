c30f84bbf2f9365db41e08a75f23f07cdc7deaac
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-20 22:47:54 -0500

    Create a component for the round, it will be wrapper with the container

--- src/shared/containers/round.jsx
+++ src/shared/containers/round.jsx
@@ -4,74 +4,14 @@ import { boardSelector, handSelector, opponentHandSelector, scoreSelector, winne
 import * as Actions from './../actions/';
 
 import React from 'react';
-import Board from './../components/board';
-import Hand from './../components/hand';
-import WINNER from './../constants/winner';
+import Round from './../components/round';
 
-class Round extends React.Component {
+class RoundContainer extends React.Component {
 
     render() {
-        let { game, board, hand, opponentHand, settings, validPieces, score, winner } = this.props;
-        let { selectCard, selectedPieceByClick, updateRoute } = this.props;
-
-        let phrase = "";
-        let handStyles={visibility: 'hidden'};
-        let bannerStyle={};
-        let bannerScrimStyle={};
-
-        switch(winner){
-            case WINNER.NONE:
-                handStyles.visibility = 'visible';
-                bannerStyle.display = 'none';
-            case WINNER.TIE:
-                phrase = "Tie";
-                bannerScrimStyle.background = 'repeating-linear-gradient(45deg, #5d9634, #5d9634 10px, #538c2b 10px, #538c2b 20px)';
-                break;
-            case WINNER.BLUE:
-                phrase = "Player 1";
-                bannerScrimStyle.background = 'repeating-linear-gradient(45deg, #606dbc, #606dbc 10px, #465298 10px, #465298 20px)';
-                break;
-            case WINNER.RED:
-                phrase = "Player 2";
-                bannerScrimStyle.background = 'repeating-linear-gradient(45deg, red, red 10px, #FF2850 10px, #FF2850 20px)';
-                break;
-        }
-
         return (
-            <div id="round">
-                <div className="row">
-                    <div className="col-xl-1 col-lg-2 col-md-2 col-sm-2 col-xs-12" style={handStyles}>
-                        <Hand score={score.blue} cards={hand} selectedCard={game.get('selectedCard')} showBack={false} clickAction={selectCard} />
-                    </div>
-
-                    <div className="col-xl-10 col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                        <Board cards={board} validPieces={validPieces} selectedPiece={game.get('selectedPiece')} selectedPieceByClick={selectedPieceByClick} />
-                    </div>
-
-                    <div className="col-xl-1 col-lg-2 col-md-2 col-sm-2 col-xs-12" style={handStyles}>
-                        <Hand score={score.red} cards={opponentHand} selectedCard={game.get('selectedCard')} showBack={settings.get('visibleHand')} clickAction={() => {}} />
-                    </div>
-                </div>
-                <div style={bannerStyle}>
-                    <div id="banner-inner" className="row">
-                        <div className="col-md-12">
-                            <div className="results row">
-                                <div className="col-md-1">{score.blue}</div>
-                                <div className="col-md-10">{phrase}</div>
-                                <div className="col-md-1">{score.red}</div>
-                            </div>
-
-                            <div className="row">
-                                <div className="col-md-12">
-                                    <button className="btn btn-next" onClick={this.props.updateRoute}>Play again</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="banner-scrim" className="row" style={bannerScrimStyle}></div>
-                </div>
-            </div>
-        );
+            <Round {...this.props} />
+        )
     }
 }
 
@@ -96,4 +36,4 @@ function mapDispatchToProps(dispatch) {
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Round);
+export default connect(mapStateToProps, mapDispatchToProps)(RoundContainer);
