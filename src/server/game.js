26ead07d25115e61d7b39fe107d348b7867b64a9
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-01-24 11:10:54 -0500

    Add stylesheet to the intial payload

--- src/server/game.js
+++ src/server/game.js
@@ -36,6 +36,7 @@ export default function game(request, response) {
             <link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png" />
             <link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png" />
             <link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png" />
+            <link rel="stylesheet" type="text/css" href="styles.css" />
           </head>
           <body>
             <div id="app">${componentHTML}</div>
