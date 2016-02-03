df9d48a84b58e3794401d56c94a8a1802ea5949c
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-03 16:32:16 -0500

    Add mailgun keys in .env and use dotenv to map key values pairs in .env file to process.env

--- src/shared/config/environment.js
+++ src/shared/config/environment.js
@@ -1,11 +1,16 @@
-const config = {
+import configureEnvironment from 'dotenv';
+configureEnvironment.config();
+
+export default {
     host: process.env.HOST || 'localhost',
     port: process.env.PORT || 3000,
     devPort: process.env.PORT || 3001,
     apiHost: process.env.HOST || 'localhost',
     apiPort: process.env.PORT || 3002,
     nodeEnv: process.env.NODE_ENV,
-    mongoUri: process.env.MONGOLAB_URI || `mongodb://localhost/${process.env.PORT || 3000}/test` || 'mongodb://db'
+    mongoUri: process.env.MONGOLAB_URI || `mongodb://localhost/${process.env.PORT || 3000}/test` || 'mongodb://db',
+    mailgun: {
+        apiKey: process.env.MAILGUN_API_KEY,
+        domain: process.env.MAILGUN_DOMAIN
+    }
 };
-
-export default config;
