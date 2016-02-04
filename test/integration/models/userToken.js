2f02389adbb91971c7e63d23000b1b5ec936246c
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-04 10:37:43 -0500

    Assert that when generating a new token it contains a token it contains a userid equal to the provided id. it does not throw an error

---
+++ test/integration/models/userToken.js
@@ -0,0 +1,21 @@
+import expect from 'expect';
+import UserToken from './../../../src/server/models/userTokens';
+
+describe('User tokens', () => {
+
+    describe('new user token', () => {
+
+        let userId;
+        beforeEach(() => {
+            userId = '0000000001';
+        });
+
+        it('should generate a token contaning the user id', () => {
+            UserToken.new(userId, (err, userToken) => {
+                expect.toNotExist(err);
+                expect.toExist(userToken.token);
+                expect(userToken.userId.toString()).toEqual(userId);
+            });
+        });
+    });
+});
