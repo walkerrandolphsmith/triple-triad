00baed2548a458943ea9f51bb5ba309bac65ae0c
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-04 20:23:09 -0500

    Assert that resenedEmailVerficiation is a function Assert that resendEmailVerfication request, receive, failure, and clear are action creators with empty payloads Assert that the user reducer handles each of the above actions but updating the resending, resent, failedToSend states

--- test/unit/reducers/user.reducer.spec.js
+++ test/unit/reducers/user.reducer.spec.js
@@ -2,6 +2,10 @@ import expect from 'expect';
 import { Map } from 'immutable';
 import reducer from './../../../src/shared/reducers/user';
 import {
+    RESEND_EMAIL_VERIFICATION,
+    RESEND_EMAIL_VERIFICATION_SUCCESS,
+    RESEND_EMAIL_VERIFICATION_FAILED,
+    RESEND_EMAIL_VERIFICATION_CLEAR,
     USER_PROFILE,
     USER_PROFILE_SUCCESS,
     } from './../../../src/shared/constants/actionTypes';
@@ -15,7 +19,10 @@ describe("User reducer", () => {
             loaded: false,
             user: new Map({
                 verified: null
-            })
+            }),
+            resending: false,
+            resendingSuccess: false,
+            resendingFailure: false
         });
     });
 
@@ -54,6 +61,54 @@ describe("User reducer", () => {
             expect(newState.get('loading')).toEqual(false);
             expect(newState.get('loaded')).toEqual(true);
             expect(newState.get('user').get('verified')).toEqual(user.verified);
+        });
+    });
+
+    describe("Given resending the verification email is initiated", () => {
+
+        it('should handle RESEND_EMAIL_VERIFICATION action', () => {
+            let newState = reducer(initialState, {
+                type: RESEND_EMAIL_VERIFICATION
+            });
+            expect(newState.get('resending')).toEqual(true);
+            expect(newState.get('resendingSuccess')).toEqual(false);
+            expect(newState.get('resendingFailure')).toEqual(false);
+        });
+    });
+
+    describe("Given resending the verification email was successful", () => {
+
+        it('should handle RESEND_EMAIL_VERIFICATION_SUCCESS action', () => {
+            let newState = reducer(initialState, {
+                type: RESEND_EMAIL_VERIFICATION_SUCCESS
+            });
+            expect(newState.get('resending')).toEqual(false);
+            expect(newState.get('resendingSuccess')).toEqual(true);
+            expect(newState.get('resendingFailure')).toEqual(false);
+        });
+    });
+
+    describe("Given resending the verification email was unsuccessful", () => {
+
+        it('should handle RESEND_EMAIL_VERIFICATION_FAILED action', () => {
+            let newState = reducer(initialState, {
+                type: RESEND_EMAIL_VERIFICATION_FAILED
+            });
+            expect(newState.get('resending')).toEqual(false);
+            expect(newState.get('resendingSuccess')).toEqual(false);
+            expect(newState.get('resendingFailure')).toEqual(true);
+        });
+    });
+
+    describe("Given the resending of email verification state is cleared ", () => {
+
+        it('should handle RESEND_EMAIL_VERIFICATION_CLEAR action', () => {
+            let newState = reducer(initialState, {
+                type: RESEND_EMAIL_VERIFICATION_CLEAR
+            });
+            expect(newState.get('resending')).toEqual(false);
+            expect(newState.get('resendingSuccess')).toEqual(false);
+            expect(newState.get('resendingFailure')).toEqual(false);
         });
     });
 
