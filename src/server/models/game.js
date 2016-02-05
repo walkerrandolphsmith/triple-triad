7938e977e73f9c830bafad1f307d2144a4e91ed8
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-04 23:53:27 -0500

    Create a game model that has a new method, that creates a game given a player id and deck

---
+++ src/server/models/game.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+let Game;
+
+let GameSchema = new mongoose.Schema({
+    owner: { type: mongoose.Schema.ObjectId },
+    opponent: { type: mongoose.Schema.ObjectId },
+    currentPlayer: { type: mongoose.Schema.ObjectId },
+    deck: Array,
+    phase: String
+});
+
+GameSchema.statics.new = function (ownerId, deck, fn) {
+
+    let game = new Game();
+
+    game.owner = ownerId;
+    game.currentPlayer = ownerId;
+    game.deck = deck;
+    game.phase = 'settings-selection';
+
+    game.save();
+};
+
+Game = mongoose.model('UserToken', GameSchema);
+
+export default Game;
