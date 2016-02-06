fa8ceffda8e939bb76f802f3f438ad4d4f356b8c
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-05 22:36:00 -0500

    On the GamesList component create a create game button that on click fires the createGame action

--- src/shared/components/games.jsx
+++ src/shared/components/games.jsx
@@ -13,7 +13,9 @@ export default class Games extends React.Component {
         return (
             <div id="games">
                 <h2>Game List</h2>
+                <button className="btn btn-next" onClick={this.props.createGame}> New Game </button>
                 {gamesList}
+
             </div>
         );
     }
