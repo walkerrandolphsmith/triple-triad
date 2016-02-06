3a2e776a184f2d31e3bd30b43ef301a235b6c51f
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-05 22:31:28 -0500

    Assert that POST to the /api/create_game returns a status of 200 and a game with the owner and deck set to the given owner id and deck in addition to setting a currentPLayer to the owner id and the game phase to settings-selection

--- test/integration/authentication/routes.js
+++ test/integration/authentication/routes.js
@@ -379,6 +379,67 @@ describe('Passport: routes', () => {
             });
         });
 
+        describe('POST /user_profile given an invalid user id', () => {
+
+            let id;
+            beforeEach(done =>  {
+                let newUser = new User();
+                newUser.local.username = 'walker';
+                newUser.local.email = 'walkerrandolphsmith@gmail.com';
+                newUser.local.password = newUser.generateHash('password');
+                newUser.save(function(err, user) {
+                    if (err) throw err;
+                    id = user._id;
+                    UserToken.new(id, (err, userToken) => {
+                        if(err) throw err;
+                        done();
+                    });
+                });
+            });
+
+            it('should return a 500 server error status', done => {
+                request(app)
+                    .post('/api/user_profile')
+                    .send({
+                        userId: 5
+                    })
+                    .expect(500)
+                    .end((err, res) => {
+                        done();
+                    });
+            });
+        });
+
+        describe('POST /create_game given a user id and deck', () => {
+
+            let id, deck;
+            beforeEach(() =>  {
+                id = 100;
+                deck = [{id: 0}, {id: 1}];
+            });
+
+            it(`should return a status of 200 OK
+                return a game with the owner and deck set to the given owner id and deck
+                and a current player set to given owner
+                and a phase set to settings-selection`, done => {
+                request(app)
+                    .post('/api/create_game')
+                    .send({
+                        userId: id,
+                        deck: deck
+                    })
+                    .expect(200)
+                    .end((err, res) => {
+                        expect(res.body.owner).toEqual(id);
+                        expect(res.body.currentPlayer).toEqual(id);
+                        expect(res.body.deck).toEqual(deck);
+                        expect(res.body.phase).toEqual('settings-selection');
+                        done();
+                    });
+            });
+        });
+
+
 
         afterEach(done => {
             User.remove({}, () => { });
