735cce57e93ca297fdbce004433517f5cdd7484e
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-07 01:34:04 -0500

    WIP connect/mongoose/mocha thing breaking shit :/

---
+++ test/integration/models/resetToken.js
@@ -0,0 +1,22 @@
+import expect from 'expect';
+import mongoose from 'mongoose';
+import connectionManager from './../connectionManager';
+import ResetToken from './../../../src/server/models/resetTokens';
+
+describe('Reset tokens', () => {
+
+    beforeEach(connectionManager.connect);
+    afterEach(connectionManager.disconnect);
+
+    describe('new reset password token', () => {
+
+        let userId;
+        beforeEach(() => {
+            userId = '000000000000000000000001';
+        });
+
+        it('should generate a token containing the user id', () => {
+
+        });
+    });
+});
