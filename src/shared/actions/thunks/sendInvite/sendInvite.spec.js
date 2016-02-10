86fca0e68635643afdd903b4745061f453ceff7e
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-10 18:05:32 -0500

    Create a spec to assert that invite dispatchs endphase

---
+++ src/shared/actions/thunks/sendInvite/sendInvite.spec.js
@@ -0,0 +1,27 @@
+import expect from 'expect';
+import SendInvite from './sendInvite';
+import { sendInvite, __RewireAPI__ as sendInviteRewireAPI } from './sendInvite';
+
+describe('SEND INVITE async action creator', () => {
+
+    let dispatch, getState;
+    beforeEach(() => {
+        dispatch = expect.createSpy();
+
+        getState = () => ({ });
+
+        SendInvite.__Rewire__('endPhase', function(){
+            return 1;
+        });
+    });
+
+    it('should be a function', () => {
+        expect(sendInvite()).toBeA('function')
+    });
+
+    it('should be a dispatch endPhase', () => {
+        sendInvite()(dispatch, getState);
+        expect(dispatch).toHaveBeenCalledWith(1)
+    });
+
+});
