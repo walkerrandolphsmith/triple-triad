be514011f30636b7d2f9b90746a2044cf50195fd
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-14 13:37:31 -0500

    Assert that user reducer
    handles PASSWORD_RESET
      - request by setting passwordReset.loading to true
      - success by setting passwordReset.loaded to true
      - failure by setting passwordReset.failed to true
      - clear by setting passwordReset.* to false

--- src/shared/reducers/user/user.spec.js
+++ src/shared/reducers/user/user.spec.js
@@ -8,6 +8,10 @@ import {
     RESEND_EMAIL_VERIFICATION_CLEAR,
     USER_PROFILE,
     USER_PROFILE_SUCCESS,
+    PASSWORD_RESET_SUCCESS,
+    PASSWORD_RESET_REQUEST,
+    PASSWORD_RESET_FAILED,
+    PASSWORD_RESET_CLEAR
     } from './../../constants/actionTypes';
 
 describe("User reducer", () => {
@@ -22,7 +26,12 @@ describe("User reducer", () => {
             }),
             resending: false,
             resendingSuccess: false,
-            resendingFailure: false
+            resendingFailure: false,
+            passwordReset: new Map({
+                loading: false,
+                loaded: false,
+                failed: false
+            })
         });
     });
 
@@ -109,6 +118,54 @@ describe("User reducer", () => {
             expect(newState.get('resending')).toEqual(false);
             expect(newState.get('resendingSuccess')).toEqual(false);
             expect(newState.get('resendingFailure')).toEqual(false);
+        });
+    });
+
+    describe('requesting to reset password', () => {
+
+        it('should handle PASSWORD_RESET_REQUEST by settings the loading state to true', () => {
+            let newState = reducer(initialState, {
+                type: PASSWORD_RESET_REQUEST
+            });
+            expect(newState.get('passwordReset').get('loading')).toEqual(true);
+            expect(newState.get('passwordReset').get('loaded')).toEqual(false);
+            expect(newState.get('passwordReset').get('failed')).toEqual(false);
+        });
+    });
+
+    describe('resetting a password is successful', () => {
+
+        it('should handle PASSWORD_RESET_SUCCESS by settings the loaded state to true', () => {
+            let newState = reducer(initialState, {
+                type: PASSWORD_RESET_SUCCESS
+            });
+            expect(newState.get('passwordReset').get('loading')).toEqual(false);
+            expect(newState.get('passwordReset').get('loaded')).toEqual(true);
+            expect(newState.get('passwordReset').get('failed')).toEqual(false);
+        });
+    });
+
+    describe('resetting the password failed', () => {
+
+        it('should handle PASSWORD_RESET_FAILED by settings the failed state to true', () => {
+            let newState = reducer(initialState, {
+                type: PASSWORD_RESET_FAILED
+            });
+            expect(newState.get('passwordReset').get('loading')).toEqual(false);
+            expect(newState.get('passwordReset').get('loaded')).toEqual(false);
+            expect(newState.get('passwordReset').get('failed')).toEqual(true);
+        });
+    });
+
+    describe('clearing the password reset state', () => {
+
+        it('should handle PASSWORD_RESET_CLEAR by settings all the loaded, loading, and failed to false', () => {
+            let newState = reducer(initialState, {
+                type: PASSWORD_RESET_CLEAR
+            });
+            expect(newState.get('passwordReset').get('loading')).toEqual(false);
+            expect(newState.get('passwordReset').get('loaded')).toEqual(false);
+            expect(newState.get('passwordReset').get('failed')).toEqual(false);
         });
     });
 
