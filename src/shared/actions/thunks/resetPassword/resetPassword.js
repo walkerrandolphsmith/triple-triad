572f6340a7c435aefe751bea57961acc10cab639
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-14 13:38:22 -0500

    Reset Password thunk dispatch the request, success, failure and clear action creators

--- src/shared/actions/thunks/resetPassword/resetPassword.js
+++ src/shared/actions/thunks/resetPassword/resetPassword.js
@@ -1,11 +1,14 @@
 import request from 'superagent';
 import {
-
+    passwordResetClear,
+    passwordResetFailed,
+    passwordResetRequest,
+    passwordResetSuccess
 } from './../../action-creators';
 
 export function resetPassword(token, password, confirmPassword) {
     return dispatch => {
-        debugger;
+        dispatch(passwordResetRequest());
         return request
             .post('/api/reset_password')
             .send(JSON.stringify({
@@ -17,8 +20,14 @@ export function resetPassword(token, password, confirmPassword) {
             .set('Content-Type', 'application/json')
             .end((error, response) => {
                 if(response.status === 200){
-
+                    dispatch(passwordResetSuccess())
+                }else{
+                    dispatch(passwordResetFailed())
                 }
+
+                setTimeout(() => {
+                    dispatch(passwordResetClear())
+                }, 1000);
             });
     };
 }
