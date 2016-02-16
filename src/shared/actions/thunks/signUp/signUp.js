11d47974d13a37c5c3721cc8e3a538a6f90e91bb
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-15 23:27:26 -0500

    Perform form validation and dispatch signupFormError actions when validation fails

--- src/shared/actions/thunks/signUp/signUp.js
+++ src/shared/actions/thunks/signUp/signUp.js
@@ -1,10 +1,58 @@
 import request from 'superagent';
 import { pushPath } from 'redux-simple-router';
-import { requestSignUp, receiveUser } from './../../action-creators'
+import { requestSignUp, receiveUser, signUpFormError } from './../../action-creators';
+import {
+    isValidUsername,
+    isValidPassword,
+    passwordsMatch,
+    isValidEmail
+    } from './../../../utils/formValidation/formValidation';
 
 export function signUp(user) {
     return dispatch => {
+
         dispatch(requestSignUp());
+
+        const { username, password, confirmPassword, email } = user;
+
+        let error = false;
+
+        if(!isValidUsername(username)) {
+            dispatch(signUpFormError({
+                field: 'username',
+                error: 'Invalid Username'
+            }));
+            error = true;
+        }
+
+        if(!isValidPassword(password)){
+            dispatch(signUpFormError({
+                field: 'password',
+                error: 'Invalid Password'
+            }));
+            error = true;
+        }
+
+        if(!passwordsMatch(password, confirmPassword)){
+            dispatch(signUpFormError({
+                field: 'username',
+                error: 'Passwords must match'
+            }));
+            error = true;
+        }
+
+        if(!isValidEmail(email)){
+            dispatch(signUpFormError({
+                field: 'email',
+                error: 'Invalid email address'
+            }));
+            error = true;
+        }
+
+        if(error){
+            return
+        }
+
         return request
             .post('/api/sign_up')
             .send(JSON.stringify(user))
