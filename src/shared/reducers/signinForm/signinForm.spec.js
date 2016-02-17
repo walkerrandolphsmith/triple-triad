bc64aa37a19c689190a599bb6961534b4686e8d2
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-16 23:23:04 -0500

    Assert signinForm reducers handles
    SIGN_IN_FORM_ERROR by settings a field's error message
    SIGN_IN_FORM_ERROR_RESET by returning the initial state (every field has no error)

---
+++ src/shared/reducers/signinForm/signinForm.spec.js
@@ -0,0 +1,55 @@
+import expect from 'expect';
+import { Map } from 'immutable';
+import reducer from './signinForm';
+import {
+    SIGN_IN_FORM_ERROR,
+    SIGN_IN_FORM_ERROR_RESET
+} from './../../constants/actionTypes';
+
+describe("Sign up form errors reducer", () => {
+
+    let initialState;
+    beforeEach(() => {
+        initialState = new Map({
+            username: '',
+            password: ''
+        });
+    });
+
+    describe("Given no state", () => {
+        it('should return the initial state', () => {
+            expect(reducer(undefined, {})).toEqual(initialState)
+        });
+    });
+
+    describe("Given a field has an error", () => {
+
+        let field, error;
+        beforeEach(() => {
+            field = 'username';
+            error = 'username error';
+        });
+
+        it('should handle SIGN_IN_FORM_ERROR action', () => {
+            let newState = reducer(initialState, {
+                type: SIGN_IN_FORM_ERROR,
+                payload: {
+                    field: field,
+                    error: error
+                }
+            });
+            expect(newState.get(field)).toEqual(error);
+        });
+    });
+
+    describe("Given a the form validation is reset", () => {
+
+        it('should handle SIGN_IN_FORM_ERROR_RESET action', () => {
+            let newState = reducer(initialState, {
+                type: SIGN_IN_FORM_ERROR_RESET
+            });
+            expect(newState).toEqual(initialState);
+        });
+    });
+
+});
