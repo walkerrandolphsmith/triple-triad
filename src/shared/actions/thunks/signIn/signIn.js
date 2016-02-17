2e74fed55a4d180ac9c941e6e28fb9a796e2d4e9
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-16 23:24:43 -0500

    Singin thunk validates username and password, dispatching signinFormErrors actions if there are errors
    If there is a failure in ajax request fire the signinFormError action given the reponse from server

--- src/shared/actions/thunks/signIn/signIn.js
+++ src/shared/actions/thunks/signIn/signIn.js
@@ -1,10 +1,36 @@
 import request from 'superagent';
 import { pushPath } from 'redux-simple-router';
-import { requestSignIn, receiveSignIn } from './../../action-creators'
+import { requestSignIn, receiveSignIn, signinFormError } from './../../action-creators'
+import {
+    isValidUsername,
+    isValidPassword
+} from './../../../utils/formValidation/formValidation';
 
 export function signIn(user) {
     return dispatch => {
         dispatch(requestSignIn());
+
+        let error;
+        if(!isValidUsername(user.username)) {
+            dispatch(signinFormError({
+                field: 'username',
+                error: 'Invalid Username'
+            }));
+            error = true;
+        }
+
+        if(!isValidPassword(user.password)){
+            dispatch(signinFormError({
+                field: 'password',
+                error: 'Invalid Password'
+            }));
+            error = true;
+        }
+
+        if(error){
+            return
+        }
+
         return request
         .post('/api/sign_in')
         .send(JSON.stringify(user))
@@ -14,6 +40,9 @@ export function signIn(user) {
             if(response.status === 200) {
                 dispatch(receiveSignIn(response.body));
                 dispatch(pushPath('/games'));
+            }else{
+                let error = JSON.parse(response.text);
+                dispatch(signinFormError(error));
             }
         });
     };
