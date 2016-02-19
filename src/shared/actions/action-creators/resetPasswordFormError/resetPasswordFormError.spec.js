0fa0a01f92d047afb8cc6650fe46322c568cd536
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-18 20:43:34 -0500

    Assert that action creators for passwordResetFormError and ErrorReset return fucntions

---
+++ src/shared/actions/action-creators/resetPasswordFormError/resetPasswordFormError.spec.js
@@ -0,0 +1,23 @@
+import expect from 'expect';
+import { RESET_PASSWORD_FORM_ERROR } from './../../../constants/actionTypes';
+import { resetPasswordFormError } from './resetPasswordFormError';
+
+describe('RESET_PASSWORD_FORM_ERROR', () => {
+
+    let payload;
+    beforeEach(() => {
+        payload = {
+            field: 'username',
+            error: 'invalid username'
+        }
+    });
+
+    it('should create an action to set a form error for a given field', () => {
+        const expectedAction = {
+            type: RESET_PASSWORD_FORM_ERROR,
+            payload: payload
+        };
+        expect(resetPasswordFormError(payload)).toEqual(expectedAction)
+    });
+
+});
