7fb21f870ebe4b8c6df45c2406cc77b8f5f01e15
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-20 12:29:45 -0500

    Update the component with conditional styles based on the form error state Dispatch a form error reset in onChange event

--- src/shared/components/forgotPassword/forgotPassword.jsx
+++ src/shared/components/forgotPassword/forgotPassword.jsx
@@ -18,6 +18,8 @@ export default class ForgotPassword extends React.Component {
         if (name === 'email') {
             this.setState({ email: value });
         }
+
+        this.props.forgotPasswordFormErrorReset();
     }
 
     handleSubmit(event) {
@@ -26,10 +28,14 @@ export default class ForgotPassword extends React.Component {
     }
 
     render() {
+        let { email } = this.props.errors;
+
+        let emailFormGroupClass = `form-group ${email ? 'has-error': ''}`;
+        let emailHelpText = !email ? (<span></span>) : (<span className="help-block">{email}</span>);
         return (
             <div id="forgot-email">
                 <form onSubmit={this.handleSubmit.bind(this)}>
-                    <div className="form-group">
+                    <div className={emailFormGroupClass}>
                         <label htmlFor="email">Email</label>
                         <input
                             className="form-control"
@@ -41,6 +47,7 @@ export default class ForgotPassword extends React.Component {
                             value={this.state.email}
                             onChange={this.handleChange.bind(this)}
                         />
+                        {emailHelpText}
                     </div>
                     <button
                         className="btn btn-main"
