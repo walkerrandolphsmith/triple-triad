fa7336198d1b29a3e93a73b5dd992b5cfa0fc096
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-21 19:35:07 -0500

    Assert that when one of the validation form functions returns that a field is not valid signUpFormError is dispatched Assert that receiveUser and pushPath are dispatched when there is a status of 200 Assert that signUpFormError is dispatched when there is a status code other than 200

--- src/shared/actions/thunks/signUp/signUp.spec.js
+++ src/shared/actions/thunks/signUp/signUp.spec.js
@@ -6,44 +6,82 @@ import mocker from 'superagent-mocker';
 
 describe.only('SIGN_UP async action creator', () => {
 
-    let dispatch, mock;
+    let dispatch, mock, user;
     beforeEach(() => {
        mock = mocker(request);
        dispatch = expect.createSpy();
-       mock.clearRoutes()
+       user = {username: 'walkerrandolphsmith', password: 'password', confirmPassword: 'password', email: 'email'};
+
+        SignUp.__Rewire__('isValidUsername', function(){
+            return true;
+        });
+
+        SignUp.__Rewire__('isValidPassword', function(){
+            return true;
+        });
+
+        SignUp.__Rewire__('passwordsMatch', function(){
+            return true;
+        });
+
+        SignUp.__Rewire__('isValidEmail', function(){
+            return true;
+        });
+
+         SignUp.__Rewire__('requestSignUp', function(){
+             return 1;
+         });
+
+         SignUp.__Rewire__('signUpFormError', function(){
+             return 2;
+         });
+    });
+
+    afterEach(() => {
+        mock.clearRoutes();
     });
 
     it('should be a function', () => {
        expect(signUp()).toBeA('function')
     });
 
-    describe('Given a valid username, password, matching password and email, when requesting to sign up', () => {
-
-        let user, username, password, confirmPassword, email;
+    describe('When /sign_up is unsuccessful', () => {
         beforeEach(() => {
-            username = 'walkerrandolphsmith';
-            password = 'password';
-            confirmPassword = 'password';
-            email = 'tester@gmail.com';
+            mock.post('/api/sign_up', function(req) {
+                return {
+                    status: 500,
+                    text: `{
+                        "field": "username",
+                        "error": "Username already exists"
+                    }`
+                };
+            });
+        });
 
-            user = {username: username, password: password, confirmPassword: confirmPassword, email: email};
+        it('should dispatch signUpFormError action', done => {
+            signUp(user)(dispatch);
+            expect(dispatch).toHaveBeenCalledWith(1);
+            setTimeout(() => {
+                expect(dispatch).toHaveBeenCalledWith(2);
+                done();
+            }, 200);
+        });
+    });
 
+    describe('Given a valid username, password, matching password and email, when requesting to sign up', () => {
+        beforeEach(() => {
             mock.post('/api/sign_up', function(req) {
                 return {
                     status: 200
                 };
             });
 
-            SignUp.__Rewire__('requestSignUp', function(){
-                return 1;
-            });
-
             SignUp.__Rewire__('receiveUser', function(){
-                return 2;
+                return 3;
             });
 
             SignUp.__Rewire__('pushPath', function(){
-                return 3;
+                return 4;
             });
         });
 
@@ -51,14 +89,66 @@ describe.only('SIGN_UP async action creator', () => {
             signUp(user)(dispatch);
             expect(dispatch).toHaveBeenCalledWith(1);
             setTimeout(() => {
-                expect(dispatch).toHaveBeenCalledWith(2);
                 expect(dispatch).toHaveBeenCalledWith(3);
+                expect(dispatch).toHaveBeenCalledWith(4);
                 done();
             }, 0);
         });
     });
 
-    describe('Signing up is unsuccessful', () => {
+    describe('Given an invalid username', () => {
+        beforeEach(() => {
+            SignUp.__Rewire__('isValidUsername', function(){
+                return false;
+            });
+        });
 
+        it('should dispatch signUpFormError action', () => {
+            signUp(user)(dispatch);
+            expect(dispatch).toHaveBeenCalledWith(1);
+            expect(dispatch).toHaveBeenCalledWith(2);
+        });
+    });
+
+    describe('Given an invalid password', () => {
+        beforeEach(() => {
+            SignUp.__Rewire__('isValidPassword', function(){
+                return false;
+            });
+        });
+
+        it('should dispatch signUpFormError action', () => {
+            signUp(user)(dispatch);
+            expect(dispatch).toHaveBeenCalledWith(1);
+            expect(dispatch).toHaveBeenCalledWith(2);
+        });
+    });
+
+    describe('Given password does not match the confirm password', () => {
+        beforeEach(() => {
+            SignUp.__Rewire__('passwordsMatch', function(){
+                return false;
+            });
+        });
+
+        it('should dispatch signUpFormError action', () => {
+            signUp(user)(dispatch);
+            expect(dispatch).toHaveBeenCalledWith(1);
+            expect(dispatch).toHaveBeenCalledWith(2);
+        });
+    });
+
+    describe('Given an invalid email', () => {
+        beforeEach(() => {
+            SignUp.__Rewire__('isValidEmail', function(){
+                return false;
+            });
+        });
+
+        it('should dispatch signUpFormError action', () => {
+            signUp(user)(dispatch);
+            expect(dispatch).toHaveBeenCalledWith(1);
+            expect(dispatch).toHaveBeenCalledWith(2);
+        });
     });
 });
