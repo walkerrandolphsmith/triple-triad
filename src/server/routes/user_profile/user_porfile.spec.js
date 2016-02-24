79a85536bf4b60a85f8e7b500356d8aa76e12c38
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-24 18:12:50 -0500

    Given a request containing a user id and a response, 	When retreiving a user by id 		Then findById is called Given the findById callback is invoked 	When successfully finding a user 		then response status of 200 and send verified prop of user 	When error finding user 		then response of 500 	When no user if found 		then response of 500

---
+++ src/server/routes/user_profile/user_porfile.spec.js
@@ -0,0 +1,128 @@
+import expect from 'expect';
+import User_Profile from './user_profile';
+import { user_profile, __RewireAPI__ as user_profileRewireAPI } from './user_profile';
+
+describe('user_profile', () => {
+
+    let req, res, userId;
+    beforeEach(() => {
+        userId = 20;
+        req = {
+            body: {
+                userId: userId
+            }
+        };
+    });
+
+    describe('Given a request containing a user id, and a response', () => {
+
+        describe('When retrieving the user profile', () => {
+            let findById;
+            beforeEach(() => {
+                res = {};
+                findById = expect.createSpy();
+                User_Profile.__Rewire__('User', {
+                    findById: findById
+                });
+            });
+
+            it('should look up the user by id', () => {
+               user_profile(req, res);
+               expect(findById).toHaveBeenCalled();
+            });
+        });
+
+        describe('Given retrieving the user by id and exercising the callback', () => {
+
+
+            describe('When the user is found with no error', () => {
+                let user, status, json;
+                beforeEach(() => {
+                    user = {id: userId, local: { verified: true } };
+                    User_Profile.__Rewire__('User', {
+                        findById: (id, fn) => {
+                            fn(null, user)
+                        }
+                    });
+
+
+                    json = expect.createSpy();
+
+                    res = {
+                        status: () => ({
+                            json: json
+                        })
+                    };
+
+                    status = expect.spyOn(res, 'status').andCallThrough();
+
+                });
+
+                it('should return a status of 200 and the user\'s id and name', () => {
+                    user_profile(req, res);
+                    expect(status).toHaveBeenCalledWith(200);
+                    expect(json).toHaveBeenCalledWith({ verified: user.local.verified });
+                });
+            });
+
+            describe('When an error occurs', () => {
+                let status, send;
+                beforeEach(() => {
+                    User_Profile.__Rewire__('User', {
+                        findById: (id, fn) => {
+                            fn(new Error(), {})
+                        }
+                    });
+
+
+                    send = expect.createSpy();
+
+                    res = {
+                        status: () => ({
+                            send: send
+                        })
+                    };
+
+                    status = expect.spyOn(res, 'status').andCallThrough();
+
+                });
+
+                it('should return a status of 500', () => {
+                    user_profile(req, res);
+                    expect(status).toHaveBeenCalledWith(500);
+                    expect(send).toHaveBeenCalled();
+                });
+            });
+
+            describe('When no user is found by given id', () => {
+                let status, send;
+                beforeEach(() => {
+                    User_Profile.__Rewire__('User', {
+                        findById: (id, fn) => {
+                            fn(null, null)
+                        }
+                    });
+
+
+                    send = expect.createSpy();
+
+                    res = {
+                        status: () => ({
+                            send: send
+                        })
+                    };
+
+                    status = expect.spyOn(res, 'status').andCallThrough();
+
+                });
+
+                it('should return a status of 500', () => {
+                    user_profile(req, res);
+                    expect(status).toHaveBeenCalledWith(500);
+                    expect(send).toHaveBeenCalled();
+                });
+            });
+
+        });
+    });
+});
