cc6fd2b775bf6dcab40115ecbbd3f8cf432800b6
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-23 20:57:48 -0500

    Given a request containing a user id and a response, when retrieving games associated with the user, then find is called Given a request containing a user id and a response, when retrieving games associated with the user has no errors, then find callback returns a status 200 and the retreived games Given a request containing a user id and a response, when retrieving games associated with the user has errors, then find callback returns a status of 500 and returns no games

--- src/server/routes/get_games/get_games.spec.js
+++ src/server/routes/get_games/get_games.spec.js
@@ -29,4 +29,75 @@ describe('get_games', () => {
            expect(find).toHaveBeenCalled();
         });
     });
+
+    describe('Given a request containing a user id, when retrieving games for associated with a user throws an error', () => {
+
+        let games, send, status;
+        beforeEach(() => {
+
+            games = [1,2,3,4];
+            Get_Games.__Rewire__('Game', {
+                find: (schema, fn) => {
+                    fn(null, games)
+                }
+            });
+
+            req = {
+                body: {
+                    userId: 20
+                }
+            };
+
+            send = expect.createSpy();
+
+            res = {
+                status: () => ({
+                    send: send
+                })
+            };
+
+            status = expect.spyOn(res, 'status').andCallThrough();
+        });
+
+        it('should attempt to find a game for that user', () => {
+            get_games(req, res);
+            expect(status).toHaveBeenCalledWith(200);
+            expect(send).toHaveBeenCalledWith(games)
+        });
+    });
+
+    describe('Given a request containing a user id, when retrieving games for associated with a user throws an error', () => {
+
+        let send, status;
+        beforeEach(() => {
+
+            Get_Games.__Rewire__('Game', {
+                find: (schema, fn) => {
+                    fn(new Error(), {})
+                }
+            });
+
+            req = {
+                body: {
+                    userId: 20
+                }
+            };
+
+            send = expect.createSpy();
+
+            res = {
+                status: () => ({
+                    send: send
+                })
+            };
+
+            status = expect.spyOn(res, 'status').andCallThrough();
+        });
+
+        it('should attempt to find a game for that user', () => {
+            get_games(req, res);
+            expect(status).toHaveBeenCalledWith(500);
+            expect(send).toHaveBeenCalled()
+        });
+    });
 });
