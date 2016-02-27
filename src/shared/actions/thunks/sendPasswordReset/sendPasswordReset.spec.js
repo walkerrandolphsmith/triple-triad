6a0056b182627c4dd3cabe38353d6eeb5922cebd
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-26 21:51:33 -0500

    When requesting send password reset 	then post /api/forgot_password 	then send email 	then set Accept and Content-Type headers WHen success 	dispatch success action When fail and invalid email 	dispacthed failure action 	dispatched form error when fail and valid email 	dispatch failure action

--- src/shared/actions/thunks/sendPasswordReset/sendPasswordReset.spec.js
+++ src/shared/actions/thunks/sendPasswordReset/sendPasswordReset.spec.js
@@ -4,14 +4,126 @@ import { sendPasswordReset, __RewireAPI__ as sendPasswordResetRewireAPI } from '
 
 describe('SEND PASSWORD RESET async action creator', () => {
 
-    let dispatch, email;
+    let dispatch;
+    let email;
+    let post, send, set;
     beforeEach(() => {
         dispatch = expect.createSpy();
         email = 'walkerrandolphsmith@gmail.com';
+
+        SendPasswordReset.__Rewire__('sendPasswordResetRequest', () => 1);
+        SendPasswordReset.__Rewire__('sendPasswordResetSuccess', () => 2);
+        SendPasswordReset.__Rewire__('sendPasswordResetFailed', () => 3);
+        SendPasswordReset.__Rewire__('sendPasswordResetClear', () => 4);
+        SendPasswordReset.__Rewire__('forgotPasswordFormError', () => 5);
     });
 
     it('should be a function', () => {
         expect(sendPasswordReset()).toBeA('function')
     });
 
+    describe('Given a request is made to get reset password', () => {
+
+        beforeEach(() => {
+            let request = SendPasswordReset.__Rewire__('request', {
+                post: function(endpoint) { return this; },
+                send: function(data) { return this },
+                set: function(key, value) { return this },
+                end: (fn) => {
+                    fn(null, { status: 200 });
+                }
+            });
+
+            post = expect.spyOn(request, 'post').andCallThrough();
+            send = expect.spyOn(request, 'send').andCallThrough();
+            set = expect.spyOn(request, 'set').andCallThrough();
+
+            sendPasswordReset(email)(dispatch);
+        });
+
+        it('should request to /api/forgot_password endpoint', () => {
+            expect(post).toHaveBeenCalledWith('/api/forgot_password');
+        });
+
+        it('should send the user id with post data', () => {
+            expect(send).toHaveBeenCalledWith(JSON.stringify({ email: email }));
+        });
+
+        it('should set the Accept and Content-Type headers', () => {
+            expect(set).toHaveBeenCalled('Accept', 'application/json');
+            expect(set).toHaveBeenCalled('Content-Type', 'application/json');
+        });
+    });
+
+    describe('When reset password is successful', () => {
+        beforeEach(() => {
+            SendPasswordReset.__Rewire__('request', {
+                post: function(endpoint) { return this; },
+                send: function(data) { return this },
+                set: function(key, value) { return this },
+                end: (fn) => {
+                    fn(null, { status: 200 });
+                }
+            });
+            sendPasswordReset(email)(dispatch);
+        });
+
+        it('should dispatch sendPasswordResetRequest action', () => {
+            expect(dispatch).toHaveBeenCalledWith(1);
+        });
+
+        it('should dispatch sendPasswordResetSuccess action', () => {
+            expect(dispatch).toHaveBeenCalledWith(2);
+        });
+    });
+
+    describe('Given the email is valid, When resend email verification is unsuccessful', () => {
+        beforeEach(() => {
+            SendPasswordReset.__Rewire__('request', {
+                post: function(endpoint) { return this; },
+                send: function(data) { return this },
+                set: function(key, value) { return this },
+                end: (fn) => {
+                    fn(null, {
+                        status: 500,
+                        body: {
+                            invalidEmail: false
+                        }
+                    });
+                }
+            });
+            sendPasswordReset(email)(dispatch);
+        });
+
+        it('should dispatch sendPasswordResetFailed action', () => {
+            expect(dispatch).toHaveBeenCalledWith(3);
+        });
+    });
+
+    describe('Given the email is invalid, When resend email verification is unsuccessful', () => {
+        beforeEach(() => {
+            SendPasswordReset.__Rewire__('request', {
+                post: function(endpoint) { return this; },
+                send: function(data) { return this },
+                set: function(key, value) { return this },
+                end: (fn) => {
+                    fn(null, {
+                        status: 500,
+                        body: {
+                            invalidEmail: true
+                        }
+                    });
+                }
+            });
+            sendPasswordReset(email)(dispatch);
+        });
+
+        it('should dispatch forgotPasswordFormError action', () => {
+            expect(dispatch).toHaveBeenCalledWith(5);
+        });
+
+        it('should dispatch sendPasswordResetFailed action', () => {
+            expect(dispatch).toHaveBeenCalledWith(3);
+        });
+    });
 });
