fd0cd9a9756624d0e5e1e76d6d43e9ad31ea0fd4
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-26 19:36:33 -0500

    Remove superagnet-mocker

--- src/shared/actions/thunks/signOut/signOut.spec.js
+++ src/shared/actions/thunks/signOut/signOut.spec.js
@@ -1,64 +1,81 @@
 import expect from 'expect';
 import SignOut from './signOut';
 import { signOut, __RewireAPI__ as signOutRewireAPI } from './signOut';
-import request from 'superagent';
-import mocker from 'superagent-mocker';
 
 describe('SIGN_OUT async action creator', () => {
 
-    let mock, dispatch;
+    let dispatch, get;
     beforeEach(() => {
-       mock = mocker(request);
        dispatch = expect.createSpy();
-
-        SignOut.__Rewire__('requestSignOut', () => {
-            return 1;
-        });
-
-        SignOut.__Rewire__('receiveSignOut', () => {
-            return 2;
-        });
-
-        SignOut.__Rewire__('pushPath', () => {
-            return 3;
-        });
-    });
-
-    afterEach(() => {
-        mock.clearRoutes();
+       SignOut.__Rewire__('requestSignOut', () => 1);
+       SignOut.__Rewire__('receiveSignOut', () => 2);
+       SignOut.__Rewire__('pushPath', () => 3);
     });
 
     it('should be a function', () => {
        expect(signOut()).toBeA('function')
     });
 
-    describe('Signing out is successful', () => {
+    describe('Given a request is made to sign out', () => {
+
         beforeEach(() => {
-            mock.get('/api/sign_out', (req) => ({
-                status: 200
-            }));
+            let request = SignOut.__Rewire__('request', {
+                get: function(endpoint) { return this; },
+                end: (fn) => {
+                    fn(null, { status: 200 });
+                }
+            });
+
+            get = expect.spyOn(request, 'get').andCallThrough();
+
+            signOut()(dispatch);
         });
 
-        it('should dispatch requestSignOut and receiveSignOut action', done => {
+        it('should request to /api/sign_out endpoint', () => {
+            expect(get).toHaveBeenCalledWith('/api/sign_out');
+        });
+    });
+
+    describe('Signing out is successful', () => {
+        beforeEach(() => {
+            let request = SignOut.__Rewire__('request', {
+                get: function(endpoint) { return this; },
+                send: function(data) { return this },
+                set: function(key, value) { return this },
+                end: (fn) => {
+                    fn(null, { status: 200 });
+                }
+            });
             signOut()(dispatch);
+        });
+
+        it('should dispatch requestSignOut action', () => {
             expect(dispatch).toHaveBeenCalledWith(1);
-            setTimeout(() => {
-                expect(dispatch).toHaveBeenCalledWith(2);
-                expect(dispatch).toHaveBeenCalledWith(3);
-                done();
-            }, 0);
+        });
+
+        it('should dispatch receiveSignOut action', () => {
+            expect(dispatch).toHaveBeenCalledWith(2);
+        });
+
+        it('should dispatch push path action', () => {
+            expect(dispatch).toHaveBeenCalledWith(3);
         });
     });
 
     describe('Signing out is unsuccessful', () => {
         beforeEach(() => {
-            mock.get('/api/sign_out', (req) => ({
-                status: 500
-            }));
+            let request = SignOut.__Rewire__('request', {
+                get: function(endpoint) { return this; },
+                send: function(data) { return this },
+                set: function(key, value) { return this },
+                end: (fn) => {
+                    fn(null, { status: 500 });
+                }
+            });
+            signOut()(dispatch);
         });
 
         it('should dispatch requestSignOut action', () => {
-            signOut()(dispatch);
             expect(dispatch).toHaveBeenCalledWith(1);
         });
     });
