3df196ad5d6d9a0c1cf20c3c4c2159878dfc74b5
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-28 13:44:09 -0500

    When sending an invite email 	then game is looked up 	When an error occurs or no game found 		then status 500 returned 	When a game is found 		then a token is attempted to be created 	When there is an error creating the token 		status 500 returned 	WHen there is a token created 		then a send email is called

---
+++ src/server/routes/invite/invite.spec.js
@@ -0,0 +1,177 @@
+import expect from 'expect';
+import Invite from './invite';
+import { invite, __RewireAPI__ as inviteRewireAPI } from './invite';
+
+describe.only('/api/invite', () => {
+
+    let req, res;
+
+    describe('Given a request containing a game id and a response', () => {
+
+        let findById = expect.createSpy();
+        beforeEach(() => {
+            req = {
+                body: {
+                    gameId: 20
+                }
+            };
+            res = {
+                status: function() { return this; },
+                send: function() { return this; }
+            };
+            Invite.__Rewire__('Game', {
+                findById: findById
+            });
+
+            invite(req, res);
+        });
+
+        it('should try to find one game', () => {
+            expect(findById).toHaveBeenCalled();
+        });
+    });
+
+    describe('Given a game is not found', () => {
+        let status, send;
+        beforeEach(() => {
+            req = {
+                body: {
+                    gameId: 20
+                }
+            };
+
+            send = expect.createSpy();
+            res = {
+                status: () => ({
+                    send: send
+                })
+            };
+            status = expect.spyOn(res, 'status').andCallThrough();
+
+            Invite.__Rewire__('Game', {
+                findById: (schema, cb) => {
+                    cb(null, null)
+                }
+            });
+
+            invite(req, res);
+        });
+
+        it('should return a status of 500', () => {
+            expect(status).toHaveBeenCalledWith(500);
+            expect(send).toHaveBeenCalled();
+        });
+    });
+
+    describe('Given an error is thrown when retrieving a game', () => {
+        let status, send;
+        beforeEach(() => {
+            req = {
+                body: {
+                    gameId: 20
+                }
+            };
+
+            send = expect.createSpy();
+            res = {
+                status: () => ({
+                    send: send
+                })
+            };
+            status = expect.spyOn(res, 'status').andCallThrough();
+
+            Invite.__Rewire__('Game', {
+                findById: (schema, cb) => {
+                    cb(new Error(), {})
+                }
+            });
+
+            invite(req, res);
+        });
+
+        it('should return a status of 500', () => {
+            expect(status).toHaveBeenCalledWith(500);
+            expect(send).toHaveBeenCalled();
+        });
+    });
+
+    describe('Given a game is found', () => {
+        let status, send;
+        beforeEach(() => {
+            req = {
+                body: {
+                    gameId: 20
+                }
+            };
+
+            send = expect.createSpy();
+            res = {
+                status: () => ({
+                    send: send
+                })
+            };
+            status = expect.spyOn(res, 'status').andCallThrough();
+
+            Invite.__Rewire__('Game', {
+                findById: (schema, cb) => {
+                    cb(null, {})
+                }
+            });
+        });
+
+        describe('When creating a token', () => {
+            let newSpy;
+            beforeEach(() => {
+                newSpy = expect.createSpy();
+                Invite.__Rewire__('Token', {
+                    new: newSpy
+                });
+
+                invite(req, res);
+            });
+
+            it('should call new', () => {
+                expect(newSpy).toHaveBeenCalled();
+            });
+        });
+
+        describe('When there is an error creating a token', () => {
+            let newSpy;
+            beforeEach(() => {
+                newSpy = expect.createSpy();
+                Invite.__Rewire__('Token', {
+                    new: (id, type, cb) => {
+                        cb(new Error(), {})
+                    }
+                });
+
+                invite(req, res);
+            });
+
+            it('should return a status of 500', () => {
+                expect(status).toHaveBeenCalledWith(500);
+                expect(send).toHaveBeenCalled();
+            });
+        });
+
+        describe('When a game is found', () => {
+            let newSpy, sendInvite;
+            beforeEach(() => {
+                newSpy = expect.createSpy();
+                Invite.__Rewire__('Token', {
+                    new: (id, type, cb) => {
+                        cb(null, {})
+                    }
+                });
+                sendInvite = expect.createSpy();
+                Invite.__Rewire__('send_invite_email', sendInvite);
+
+                invite(req, res);
+            });
+
+            it('should send an email', () => {
+                expect(sendInvite).toHaveBeenCalled()
+            });
+        });
+    });
+});
