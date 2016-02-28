86b9c1013930874dbf430088dcd3ef86bf776e54
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-02-28 13:46:52 -0500

    When sending a invite email, 	then a game is looked up. 	WHen game is not found or error return status 500 	When game is found 		then attempt to create token 		When error return 500 status 		WHen token created successfully 			then send email

--- src/server/routes/invite/invite.js
+++ src/server/routes/invite/invite.js
@@ -1,3 +1,24 @@
+import Game from './../../models/game/game';
+import Token from './../../models/token/token';
+import { send_invite_email } from './../../utils/mailer/mailer';
+
 export function invite(req, res, next) {
-    res.status(200).json({});
+    const gameId = req.body.gameId;
+    Game.findById(gameId, (err, game) => {
+        if(err || game === null){
+            res.status(500).send();
+        }
+        else{
+            Token.new(game._id, 'INVITE', (err, token) => {
+                if(err){
+                    res.status(500).send();
+                }
+                else{
+                    send_invite_email('email', 'token', err => {
+
+                    });
+                }
+            });
+        }
+    });
 }
