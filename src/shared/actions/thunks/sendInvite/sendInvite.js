1bf24b54e48482a0faf118ab1f068b0e88b24eff
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-03-04 18:51:30 -0500

    Make a request to api/invite when send invite is dispatched

--- src/shared/actions/thunks/sendInvite/sendInvite.js
+++ src/shared/actions/thunks/sendInvite/sendInvite.js
@@ -4,5 +4,19 @@ import { endPhase } from './../endPhase/endPhase';
 export function sendInvite(email) {
     return dispatch => {
         dispatch(endPhase());
+
+
+        const data = {
+          email: email
+        };
+
+        return request
+        .post('/api/invite')
+        .send(JSON.stringify(data))
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .end((error, response) => {
+
+        });
     };
 }
