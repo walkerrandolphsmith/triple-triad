bef85db321a2c2f5a615bf6da852388eaef0d06d
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-03-04 18:51:05 -0500

    When send invite thunk is dispatched, 	then a request is made to /api/invite 		with email address 		and Content-Type and Accept headers

--- src/shared/actions/thunks/sendInvite/sendInvite.spec.js
+++ src/shared/actions/thunks/sendInvite/sendInvite.spec.js
@@ -24,4 +24,39 @@ describe('SEND INVITE async action creator', () => {
         expect(dispatch).toHaveBeenCalledWith(1)
     });
 
+    describe('Given a request is made to get reset password', () => {
+        let post, send, set;
+        let email;
+        beforeEach(() => {
+            let request = SendInvite.__Rewire__('request', {
+                post: function(endpoint) { return this; },
+                send: function(data) { return this },
+                set: function(key, value) { return this },
+                end: (fn) => {
+                    fn(null, { status: 200 });
+                }
+            });
+
+            post = expect.spyOn(request, 'post').andCallThrough();
+            send = expect.spyOn(request, 'send').andCallThrough();
+            set = expect.spyOn(request, 'set').andCallThrough();
+
+            email = 'tester@gmail.com';
+            sendInvite(email)(dispatch);
+        });
+
+        it('should request to /api/invite endpoint', () => {
+            expect(post).toHaveBeenCalledWith('/api/invite');
+        });
+
+        it('should send the user id with post data', () => {
+            expect(send).toHaveBeenCalledWith(JSON.stringify({ email: email }));
+        });
+
+        it('should set the Accept and Content-Type headers', () => {
+            expect(set).toHaveBeenCalled('Accept', 'application/json');
+            expect(set).toHaveBeenCalled('Content-Type', 'application/json');
+        });
+    });
+
 });
