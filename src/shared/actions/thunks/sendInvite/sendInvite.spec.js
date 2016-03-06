817350b1faf06b012750720afa17060b07815ad8
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-03-06 10:14:57 -0500

    Given the request to send invite is made 	then dispatch sendInviteRequest 	When the status code is 200 		then dispatch sendInviteSuccess 	When the status code is not 200 		then dispatch sendInviteFailed

--- src/shared/actions/thunks/sendInvite/sendInvite.spec.js
+++ src/shared/actions/thunks/sendInvite/sendInvite.spec.js
@@ -13,6 +13,10 @@ describe('SEND INVITE async action creator', () => {
         SendInvite.__Rewire__('endPhase', () => {
             return 1;
         });
+
+        SendInvite.__Rewire__('sendInviteRequest', () => 1);
+        SendInvite.__Rewire__('sendInviteFailed', () => 2);
+        SendInvite.__Rewire__('sendInviteSuccess', () => 3);
     });
 
     it('should be a function', () => {
@@ -56,6 +60,56 @@ describe('SEND INVITE async action creator', () => {
         it('should set the Accept and Content-Type headers', () => {
             expect(set).toHaveBeenCalled('Accept', 'application/json');
             expect(set).toHaveBeenCalled('Content-Type', 'application/json');
+        });
+    });
+
+    describe('Given a request is made and is successful', () => {
+        let email;
+        beforeEach(() => {
+            let request = SendInvite.__Rewire__('request', {
+                post: function(endpoint) { return this; },
+                send: function(data) { return this },
+                set: function(key, value) { return this },
+                end: (fn) => {
+                    fn(null, { status: 200 });
+                }
+            });
+
+            email = 'tester@gmail.com';
+            sendInvite(email)(dispatch);
+        });
+
+        it('should dispatch sendInviteRequest action', () => {
+            expect(dispatch).toHaveBeenCalledWith(1);
+        });
+
+        it('should dispatch sendInviteSuccess action', () => {
+            expect(dispatch).toHaveBeenCalledWith(3);
+        });
+    });
+
+    describe('Given a request is made and is unsuccessful', () => {
+        let email;
+        beforeEach(() => {
+            let request = SendInvite.__Rewire__('request', {
+                post: function(endpoint) { return this; },
+                send: function(data) { return this },
+                set: function(key, value) { return this },
+                end: (fn) => {
+                    fn(null, { status: 500 });
+                }
+            });
+
+            email = 'tester@gmail.com';
+            sendInvite(email)(dispatch);
+        });
+
+        it('should dispatch sendInviteRequest action', () => {
+            expect(dispatch).toHaveBeenCalledWith(1);
+        });
+
+        it('should dispatch sendInviteFailed action', () => {
+            expect(dispatch).toHaveBeenCalledWith(2);
         });
     });
 
