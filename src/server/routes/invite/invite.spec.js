0c11dce35f981b1c81476f3a292b879ebef8d9d0
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-03-08 20:08:46 -0500

    When a game is found, a token is successfully create 	then a user is looked up by id 		When a user is found attempt to send email 		When a user is not found or error return status 500

--- src/server/routes/invite/invite.spec.js
+++ src/server/routes/invite/invite.spec.js
@@ -154,72 +154,112 @@ describe('/api/invite', () => {
             });
         });
 
-        describe('When a game is found', () => {
-            let newSpy, sendInvite;
+
+        describe('Given a game is found, and a token is successfully created', () => {
             beforeEach(() => {
-                newSpy = expect.createSpy();
                 Invite.__Rewire__('Token', {
                     new: (id, type, cb) => {
                         cb(null, {})
                     }
                 });
-                sendInvite = expect.createSpy();
-                Invite.__Rewire__('send_invite_email', sendInvite);
-
-                invite(req, res);
             });
 
-            it('should send an email', () => {
-                expect(sendInvite).toHaveBeenCalled()
-            });
-        });
-
-        describe('When sending an email', () => {
-            let newSpy, sendInvite;
-            beforeEach(() => {
-                newSpy = expect.createSpy();
-                Invite.__Rewire__('Token', {
-                    new: (id, type, cb) => {
-                        cb(null, {})
-                    }
-                });
-                sendInvite = expect.createSpy();
-                Invite.__Rewire__('send_invite_email', sendInvite);
-            });
-            describe('When email is sent without error', () => {
-                let json;
+            describe('When looking up the email of the sender of the invite', () => {
+                let findById;
                 beforeEach(() => {
-                    Invite.__Rewire__('send_invite_email', (toEmail, fromEmail, token, cb) => {
-                        cb(null);
-                    });
-                    json = expect.createSpy();
-                    res = {
-                        status: () => ({
-                            json: json
-                        })
-                    };
-                    status = expect.spyOn(res, 'status').andCallThrough();
-                    invite(req, res);
-                });
-
-                it('should respond with json', () => {
-                    expect(status).toHaveBeenCalledWith(200);
-                    expect(json).toHaveBeenCalledWith({ sent: true })
-                })
-            });
-
-            describe('When there is an error sending email', () => {
-                beforeEach(() => {
-                    Invite.__Rewire__('send_invite_email', (toEmail, fromEmail, token, cb) => {
-                        cb(new Error());
+                    findById = expect.createSpy();
+                    Invite.__Rewire__('User', {
+                        findById: findById
                     });
                     invite(req, res);
                 });
 
-                it('should respond with status 500', () => {
-                    expect(status).toHaveBeenCalledWith(500);
-                    expect(send).toHaveBeenCalled();
-                })
+                it('should attempt to look up a user given the id', () => {
+                    expect(findById).toHaveBeenCalled()
+                });
+            });
+
+            describe('Given the user can not be found', () => {
+
+                describe('When sending the email', () => {
+                    let sendInvite;
+                    beforeEach(() => {
+                        Invite.__Rewire__('User', {
+                            findById: (id, cb) => {
+                                cb(new Error(), null)
+                            }
+                        });
+                        invite(req, res);
+                    });
+
+                    it('should attempt to send an invite email', () => {
+                        expect(status).toHaveBeenCalledWith(500);
+                        expect(send).toHaveBeenCalled();
+                    });
+                });
+            });
+
+            describe('Given the user can be found', () => {
+
+                describe('When sending the email', () => {
+                    let sendInvite;
+                    beforeEach(() => {
+                        Invite.__Rewire__('User', {
+                            findById: (id, cb) => {
+                                cb(null, {
+                                    local: {
+                                        email: 'tester@gmail.com'
+                                    }
+                                })
+                            }
+                        });
+                        sendInvite = expect.createSpy();
+                        Invite.__Rewire__('send_invite_email', sendInvite);
+                        invite(req, res);
+                    });
+
+                    it('should attempt to send an invite email', () => {
+                        expect(sendInvite).toHaveBeenCalled()
+                    });
+
+                    describe('When sending an email', () => {
+                        describe('When email is sent without error', () => {
+                            let json;
+                            beforeEach(() => {
+                                Invite.__Rewire__('send_invite_email', (toEmail, fromEmail, token, cb) => {
+                                    cb(null);
+                                });
+                                json = expect.createSpy();
+                                res = {
+                                    status: () => ({
+                                        json: json
+                                    })
+                                };
+                                status = expect.spyOn(res, 'status').andCallThrough();
+                                invite(req, res);
+                            });
+
+                            it('should respond with json', () => {
+                                expect(status).toHaveBeenCalledWith(200);
+                                expect(json).toHaveBeenCalledWith({ sent: true })
+                            })
+                        });
+
+                        describe('When there is an error sending email', () => {
+                            beforeEach(() => {
+                                Invite.__Rewire__('send_invite_email', (toEmail, fromEmail, token, cb) => {
+                                    cb(new Error());
+                                });
+                                invite(req, res);
+                            });
+
+                            it('should respond with status 500', () => {
+                                expect(status).toHaveBeenCalledWith(500);
+                                expect(send).toHaveBeenCalled();
+                            })
+                        });
+                    });
+                });
             });
         });
     });
