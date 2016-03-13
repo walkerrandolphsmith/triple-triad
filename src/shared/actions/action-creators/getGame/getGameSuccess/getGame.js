aee9df7a92ccf32862df0764d752126c40bee283
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-03-12 21:54:43 -0500

    Action creator that creates an action with type GET_GAME_SUCCESS

---
+++ src/shared/actions/action-creators/getGame/getGameSuccess/getGame.js
@@ -0,0 +1,10 @@
+import { GET_GAME_SUCCESS } from './../../../../constants/actionTypes';
+
+export default function(){
+    return {
+        type: GET_GAME_SUCCESS,
+        payload: {
+
+        }
+    }
+}
