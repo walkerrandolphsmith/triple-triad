4d9b5dfbdc58ea54a952daa0a8bb9e17e00c3619
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-03-13 21:36:26 -0400

    Assert that the request is made to the /api/get_game endpoint, given a game id, with the correct headers. Assert that getGameRequest is dispatched. Assert that given a 200 status getGameSuccess is dispatched and other getGameFailed is dispatched.

---
+++ src/shared/actions/thunks/getGame/getGame.spec.js
@@ -0,0 +1,100 @@
+import expect from 'expect';
+import { Map } from 'immutable';
+import GetGame from './getGame';
+import { getGame, __RewireAPI__ } from './getGame';
+
+describe('Get Games async action creator', () => {
+
+    let dispatch, getState, gameId;
+    let post, send, set;
+    beforeEach(() => {
+        gameId = 20;
+        dispatch = expect.createSpy();
+        getState = () => ({
+          auth: new Map({
+              user: new Map({
+                  id: gameId
+              })
+          })
+        });
+
+        GetGame.__Rewire__('getGameRequest', () => 1);
+        GetGame.__Rewire__('getGameSuccess', () => 2);
+        GetGame.__Rewire__('getGameFailed', () => 3);
+    });
+
+    it('should be a function', () => {
+       expect(getGame(gameId)).toBeA('function')
+    });
+
+    describe('Given a request is made', () => {
+
+        beforeEach(() => {
+            let request = GetGame.__Rewire__('request', {
+                post: function(endpoint) { return this; },
+                send: function(data) { return this },
+                set: function(key, value) { return this },
+                end: (fn) => {
+                    fn(null, { status: 200 });
+                }
+            });
+
+            post = expect.spyOn(request, 'post').andCallThrough();
+            send = expect.spyOn(request, 'send').andCallThrough();
+            set = expect.spyOn(request, 'set').andCallThrough();
+
+            getGame(gameId)(dispatch, getState);
+        });
+
+        it('should request to /api/get_game endpoint', () => {
+            expect(post).toHaveBeenCalledWith('/api/get_game');
+        });
+
+        it('should send the game id with post data', () => {
+            expect(send).toHaveBeenCalledWith(JSON.stringify({gameId: gameId}));
+        });
+
+        it('should set the Accept and Content-Type headers', () => {
+            expect(set).toHaveBeenCalled('Accept', 'application/json');
+            expect(set).toHaveBeenCalled('Content-Type', 'application/json');
+        });
+    });
+
+    describe('When /get_game is successful', () => {
+        beforeEach(() => {
+            GetGame.__Rewire__('request', {
+                post: function(endpoint) { return this; },
+                send: function(data) { return this },
+                set: function(key, value) { return this },
+                end: (fn) => {
+                    fn(null, { status: 200 });
+                }
+            });
+            getGame(gameId)(dispatch, getState);
+        });
+
+        it('should dispatch getGameSuccess action', () => {
+            expect(dispatch).toHaveBeenCalledWith(1);
+            expect(dispatch).toHaveBeenCalledWith(2);
+        });
+    });
+
+    describe('When /get_game is unsuccessful', () => {
+        beforeEach(() => {
+            GetGame.__Rewire__('request', {
+                post: function(endpoint) { return this; },
+                send: function(data) { return this },
+                set: function(key, value) { return this },
+                end: (fn) => {
+                    fn(null, { status: 500 });
+                }
+            });
+            getGame(gameId)(dispatch, getState);
+        });
+
+        it('should dispatch getGameFailed action', () => {
+            expect(dispatch).toHaveBeenCalledWith(1);
+            expect(dispatch).toHaveBeenCalledWith(3);
+        });
+    });
+});
