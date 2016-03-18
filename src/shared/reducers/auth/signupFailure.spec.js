35d8052d76a12bd471db9b05b9d86909574f5d31
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-03-18 00:02:21 -0400

    The rest of the tests for the auth reducer

--- src/shared/reducers/auth/signupFailure.spec.js
+++ src/shared/reducers/auth/signupFailure.spec.js
@@ -1,16 +1,12 @@
 import expect from 'expect';
 import { Map } from 'immutable';
 
-import signinFailure from './signinFailure';
+import signupFailure from './signupFailure';
 
 describe('Given authentication state', () => {
     let state;
     beforeEach(() => {
         state = new Map({
-            loading: false,
-            loaded: false,
-            signingIn: false,
-            signingOut: false,
             signingUp: false,
             user: new Map({
                 username: null,
@@ -20,17 +16,17 @@ describe('Given authentication state', () => {
     });
 
 
-    describe('When signing in fails', () => {
+    describe('When signing up fails', () => {
         let actual;
         let error;
         beforeEach(() => {
             error = 'a';
-            actual = signinFailure(state, {error: error})
+            actual = signupFailure(state, {error: error})
         });
 
 
-        it('should set the signingIn state to false', () => {
-            expect(actual.get('signingIn')).toEqual(true);
+        it('should set the signingUp state to false', () => {
+            expect(actual.get('signingUp')).toEqual(false);
         });
 
         it('should set the username to null', () => {
@@ -39,10 +35,6 @@ describe('Given authentication state', () => {
 
         it('should set the user id to true', () => {
             expect(actual.get('user').get('id')).toEqual(null);
-        });
-
-        it('should set the signingInError to the payload error', () => {
-            expect(actual.get('signingInError')).toEqual(error);
         });
     });
 });
--- src/shared/reducers/auth/signupSuccess.spec.js
+++ src/shared/reducers/auth/signupSuccess.spec.js
@@ -1,7 +1,7 @@
 import expect from 'expect';
 import { Map } from 'immutable';
 
-import signin from './signin';
+import signupSuccess from './signupSuccess';
 
 describe('Given authentication state', () => {
     let state;
@@ -20,15 +20,29 @@ describe('Given authentication state', () => {
     });
 
 
-    describe('When attempting to signin', () => {
+    describe('When signing up is successful', () => {
         let actual;
+        let name, id;
         beforeEach(() => {
-            actual = signin(state)
+            name = 'a'; id = 'b';
+            actual = signupSuccess(state, {
+                user: {
+                    id: id,
+                    name: name
+                }
+            })
         });
 
+        it('should set the signingUp state to true', () => {
+            expect(actual.get('signingUp')).toEqual(false);
+        });
 
-        it('should set the signingIn state to true', () => {
-            expect(actual.get('signingIn')).toEqual(true);
+        it('should set the username to null', () => {
+            expect(actual.get('user').get('username')).toEqual(name)
+        });
+
+        it('should set the user id to true', () => {
+            expect(actual.get('user').get('id')).toEqual(id);
         });
     });
 });
