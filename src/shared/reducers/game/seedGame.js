9a2e74e56d0727682caf381600d12e39a21241c2
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-03-25 19:14:38 -0400

    Make sure when seeding the deck from the server your deck is converted into an immutable structure

--- src/shared/reducers/game/seedGame.js
+++ src/shared/reducers/game/seedGame.js
@@ -1,7 +1,9 @@
+import { fromJS } from 'immutable';
+
 export default (state, payload) => {
     state = state.set('gameId', payload.game._id);
     state = state.set('owner', payload.game.owner);
-    state = state.set('deck', payload.game.deck);
+    state = state.set('deck', fromJS(payload.game.deck));
     state = state.set('phase', payload.game.phase);
     state = state.set('accepted', payload.game.accepted);
     return state.set('currentPlayer', payload.game.currentPlayer);
