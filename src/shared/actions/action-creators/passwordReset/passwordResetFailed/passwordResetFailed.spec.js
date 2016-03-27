3a3a1ed4daa06db9f01cd4b49503bd8819cab2ed
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-03-27 02:16:44 -0400

    Fix constant name

--- src/shared/actions/action-creators/passwordReset/passwordResetFailed/passwordResetFailed.spec.js
+++ src/shared/actions/action-creators/passwordReset/passwordResetFailed/passwordResetFailed.spec.js
@@ -1,12 +1,12 @@
 import expect from 'expect';
-import {  PASSWORD_RESET_FAILURE } from './../../../../constants/actionTypes';
+import {  PASSWORD_RESET_FAILED } from './../../../../constants/actionTypes';
 import { passwordResetFailed } from './passwordResetFailed';
 
 describe('Given PASSWORD_RESET_FAILURE action type, When invoking the passwordResetFailed action creator', () => {
 
     it('should create an action', () => {
         const expectedAction = {
-            type: PASSWORD_RESET_FAILURE
+            type: PASSWORD_RESET_FAILED
         };
         expect(passwordResetFailed()).toEqual(expectedAction)
     });
