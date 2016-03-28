d0608bc6962ee9656749a4f6cc7e909993a3761b
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-03-27 21:11:34 -0400

    Convert anoynomous functions to named functions to avoid callback hell

--- src/server/config/passport.js
+++ src/server/config/passport.js
@@ -3,14 +3,62 @@ import User from './../models/user/user';
 import Token from './../models/token/token';
 import { sendVerificationEmail } from './../utils/mailer/mailer';
 
+function sendEmailCallback(data, emailErr) {
+    let [done, newUser] = data;
+    if(emailErr) {
+        return done(emailErr);
+    }
+    return done(null, newUser);
+}
+
+function newTokenCallback(data, tokenErr, token) {
+    let [done, newUser] = data;
+    if(tokenErr) {
+        return done(tokenErr);
+    }
+    sendVerificationEmail(
+        newUser.local.email,
+        token.token,
+        sendEmailCallback.bind(sendEmailCallback, [done, newUser])
+    );
+}
+
+function saveUserCallback(data, saveErr) {
+    let [done, newUser] = data;
+    if(saveErr) {
+        return done(saveErr);
+    }
+    Token.new(
+        newUser._id,
+        'USER',
+        newTokenCallback.bind(newTokenCallback, [done, newUser])
+    );
+}
+
+function findUserCallback(data, err, user) {
+    let [req, done, username, password] = data;
+    var newUser;
+    if(err) {
+        return done(err);
+    }
+    if(user) {
+        return done(null, false, { message: 'User already exists.' });
+    }
+    newUser = new User();
+    newUser.local.username = username;
+    newUser.local.email = req.body.email;
+    newUser.local.password = newUser.generateHash(password);
+    newUser.save(saveUserCallback.bind(saveUserCallback, [done, newUser]));
+}
+
 export default function(passport) {
     const LocalStrategy = PassportLocal.Strategy;
 
-    passport.serializeUser(function(user, done) {
+    passport.serializeUser((user, done) => {
         done(null, { id: user._id, name: user.local.username });
     });
 
-    passport.deserializeUser(function(user, done) {
+    passport.deserializeUser((user, done) => {
         User.findById(user.id, function(err, u) {
             done(err, u);
         });
@@ -21,36 +69,11 @@ export default function(passport) {
         passwordField: 'password',
         passReqToCallback: true
     },
-    function(req, username, password, done) {
-        User.findOne({ 'local.username': username }, function(err, user) {
-            var newUser;
-            if(err) {
-                return done(err);
-            }
-            if(user) {
-                return done(null, false, { message: 'User already exists.' });
-            }
-            newUser = new User();
-            newUser.local.username = username;
-            newUser.local.email = req.body.email;
-            newUser.local.password = newUser.generateHash(password);
-            newUser.save(saveErr => {
-                if(saveErr) {
-                    return done(saveErr);
-                }
-                Token.new(newUser._id, 'USER', (tokenErr, token) => {
-                    if(tokenErr) {
-                        return done(tokenErr);
-                    }
-                    sendVerificationEmail(newUser.local.email, token.token, emailErr => {
-                        if(emailErr) {
-                            return done(emailErr);
-                        }
-                        return done(null, newUser);
-                    });
-                });
-            });
-        });
+    (req, username, password, done) => {
+        User.findOne(
+            { 'local.username': username },
+            findUserCallback.bind(findUserCallback, [req, done, username, password])
+        );
     }));
 
     passport.use('local-login', new LocalStrategy({
@@ -59,7 +82,7 @@ export default function(passport) {
         passReqToCallback: true
     },
     function(req, username, password, done) {
-        User.findOne({ 'local.username': username }, function(err, user) {
+        User.findOne({ 'local.username': username }, (err, user) => {
             if(err) {
                 return done(err);
             }
