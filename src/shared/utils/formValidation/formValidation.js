7eb735ea18c93a725f017cf1754f5702110c005d
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-04-17 19:54:45 -0400

    Reformat funcs to one liners

--- src/shared/utils/formValidation/formValidation.js
+++ src/shared/utils/formValidation/formValidation.js
@@ -1,15 +1,7 @@
-export function isValidPassword(password) {
-    return /^[a-z0-9_-]{6,25}$/.test(password);
-}
+export const isValidPassword = password => /^[a-z0-9_-]{6,25}$/.test(password);
 
-export function passwordsMatch(password, repeatedPassword) {
-    return password === repeatedPassword;
-}
+export const passwordsMatch = (password, repeatedPassword) => password === repeatedPassword;
 
-export function isValidUsername(username) {
-    return /^[a-z0-9_-]{3,20}$/.test(username);
-}
+export const isValidUsername = username => /^[a-z0-9_-]{3,20}$/.test(username);
 
-export function isValidEmail(email) {
-    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(email);
-}
+export const isValidEmail = email => /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(email);
