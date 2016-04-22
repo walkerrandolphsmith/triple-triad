7ba9566a6eaa9ad928e9bc10c46b8bfac708b82b
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-04-22 16:36:01 -0400

    Refactor to inline code and remove new lines

--- src/shared/actions/thunks/getNextCardForHand/getNextCardForHand.js
+++ src/shared/actions/thunks/getNextCardForHand/getNextCardForHand.js
@@ -3,11 +3,7 @@ import getCurrentGame from './../../utils/getCurrentGame';
 import { selectCard } from './../../action-creators';
 
 export const getNextCardForHand = (directionInLoop) => (dispatch, getState) => {
-    const state = getState();
-
-    const currentGame = getCurrentGame(state);
-
+    const currentGame = getCurrentGame(getState());
     const card = getCardToAdd(currentGame, directionInLoop);
-
     dispatch(selectCard(card.get('id')));
 };
