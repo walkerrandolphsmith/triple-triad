c87c50b910236f9b15772a49fb07440d63159870
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-04-24 11:30:57 -0400

    Refactor to handle getCurrentGame. Wrap the applyflips and clean up of card placement in a setTimeout to allow the socket to propagate the placement of the card.... rethink these situations

--- src/shared/actions/thunks/playerTakesTurn/playerTakesTurn.js
+++ src/shared/actions/thunks/playerTakesTurn/playerTakesTurn.js
@@ -6,19 +6,17 @@ import { getBoard } from './../../../selectors/board/boardSelector';
 import getCurrentGame from './../../utils/getCurrentGame';
 
 export const playerTakesTurn = (isPlayer) => (dispatch, getState) => {
-    const state = getState();
-    const currentGame = getCurrentGame(state);
-
-    const gameOver = getBoard(currentGame.get('deck')).size === 9;
+    const currentGame = getCurrentGame(getState());
 
     dispatch(placeCard());
 
+    //Wait for the socket to propagate the card placement
     setTimeout(() => {
         dispatch(applyFlips());
         dispatch(selectCard(-1));
         dispatch(selectPiece(-1));
 
-        if(isPlayer && !gameOver) {
+        if(shouldAiTakeTurnAfterPlayer(currentGame, isPlayer)) {
             setTimeout(() => {
                 dispatch(aiTurn());
                 dispatch(getNextSelectedCard());
@@ -26,3 +24,8 @@ export const playerTakesTurn = (isPlayer) => (dispatch, getState) => {
         }
     }, 500);
 };
+
+function shouldAiTakeTurnAfterPlayer(currentGame, isPlayer) {
+    let gameOver = getBoard(currentGame.get('deck')).size === 9;
+    return isPlayer && !gameOver
+}
--- src/shared/actions/thunks/playerTakesTurn/playerTakesTurn.spec.js
+++ src/shared/actions/thunks/playerTakesTurn/playerTakesTurn.spec.js
@@ -2,16 +2,17 @@ import expect from 'expect';
 import { Map, List } from 'immutable';
 import { playerTakesTurn, __RewireAPI__ } from './playerTakesTurn';
 
-describe('PLAYER_TAKES_TURN async action creator', () => {
+describe('src/shared/actions/thunks/playerTakesTurn', () => {
     let getState;
     let dispatch;
     let isPlayer;
+    let game;
     beforeEach(() => {
-        getState = () => ({
-            game: new Map({
-                deck: new List([])
-            })
+        game = new Map({
+            deck: new List([])
         });
+        __RewireAPI__.__Rewire__('getCurrentGame', () => game);
+        getState = () => ({});
         dispatch = expect.createSpy();
     });
 
@@ -29,36 +30,6 @@ describe('PLAYER_TAKES_TURN async action creator', () => {
             playerTakesTurn(isPlayer)(dispatch, getState);
             expect(dispatch).toHaveBeenCalledWith(1);
         });
-
-        it('should dispatch RULE async action', () => {
-            __RewireAPI__.__Rewire__('applyFlips', () => 2);
-            playerTakesTurn(isPlayer)(dispatch, getState);
-            expect(dispatch).toHaveBeenCalledWith(2);
-        });
-
-        it('should dispatch RULE async action', () => {
-            __RewireAPI__.__Rewire__('selectCard', () => 3);
-            playerTakesTurn(isPlayer)(dispatch, getState);
-            expect(dispatch).toHaveBeenCalledWith(3);
-        });
-
-        it('should dispatch RULE async action', () => {
-            __RewireAPI__.__Rewire__('selectPiece', () => 4);
-            playerTakesTurn(isPlayer)(dispatch, getState);
-            expect(dispatch).toHaveBeenCalledWith(4);
-        });
-
-        xit('should dispatch AI_TURN async action', () => {
-            __RewireAPI__.__Rewire__('aiTurn', () => 5);
-            playerTakesTurn(isPlayer)(dispatch, getState);
-            expect(dispatch).toHaveBeenCalledWith(5);
-        });
-
-        xit('should dispatch GET_NEXT_SLECTED_CARD async action', () => {
-            __RewireAPI__.__Rewire__('getNextSelectedCard', () => 6);
-            playerTakesTurn(isPlayer)(dispatch, getState);
-            expect(dispatch).toHaveBeenCalledWith(6);
-        });
     });
 
     describe('Given it is the opponent', () => {
@@ -70,24 +41,6 @@ describe('PLAYER_TAKES_TURN async action creator', () => {
             __RewireAPI__.__Rewire__('placeCard', () => 1);
             playerTakesTurn(isPlayer)(dispatch, getState);
             expect(dispatch).toHaveBeenCalledWith(1);
-        });
-
-        it('should dispatch RULE async action', () => {
-            __RewireAPI__.__Rewire__('applyFlips', () => 2);
-            playerTakesTurn(isPlayer)(dispatch, getState);
-            expect(dispatch).toHaveBeenCalledWith(2);
-        });
-
-        it('should dispatch RULE async action', () => {
-            __RewireAPI__.__Rewire__('selectCard', () => 3);
-            playerTakesTurn(isPlayer)(dispatch, getState);
-            expect(dispatch).toHaveBeenCalledWith(3);
-        });
-
-        it('should dispatch RULE async action', () => {
-            __RewireAPI__.__Rewire__('selectPiece', () => 4);
-            playerTakesTurn(isPlayer)(dispatch, getState);
-            expect(dispatch).toHaveBeenCalledWith(4);
         });
     });
 });
