9524f93b96f07039af8808133b51ef56909fef90
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-04-24 11:50:33 -0400

    Refactor to handle reducer storing all games.

--- src/shared/reducers/game/addCard.spec.js
+++ src/shared/reducers/game/addCard.spec.js
@@ -3,31 +3,46 @@ import { Map, List } from 'immutable';
 
 import addCard from './addCard';
 
-describe('Given game state and payload containing id and owner of a card', () => {
-    let state;
-    let payload;
-    beforeEach(() => {
-        state = new Map({
-            deck: new List([
-                new Map({
-                    id: 20, owner: 2
-                })
-            ])
-        });
-        payload = {
-            id: 20,
-            owner: 1
-        };
-    });
-
-    describe('When adding a card to player ownership', () => {
-        let actual;
+describe('src/shared/reducers/addCard', () => {
+    describe('Given game state and payload containing id and owner of a card', () => {
+        let state;
+        let payload;
         beforeEach(() => {
-            actual = addCard(state, payload);
+            let gameId = 1;
+            let cardId = 20;
+            let owner = 2;
+
+            let card = new Map({
+                id: cardId, owner: owner
+            });
+
+            let deck = new List([card]);
+
+            let game = new Map({
+                id: gameId,
+                deck: deck
+            });
+
+            state = new Map({
+                games: new List([game]),
+                gameRoute: gameId
+            });
+
+            payload = {
+                id: cardId,
+                owner: owner
+            };
         });
 
-        it('should set a card, in the deck with id equal to payload id, owner to the payload owner ', () => {
-            expect(actual.get('deck').first().get('owner')).toEqual(payload.owner);
+        describe('When adding a card to player ownership', () => {
+            let actual;
+            beforeEach(() => {
+                actual = addCard(state, payload);
+            });
+
+            it('should set a card, in the deck with id equal to payload id, owner to the payload owner ', () => {
+                expect(actual.get('games').first().get('deck').first().get('owner')).toEqual(payload.owner);
+            });
         });
     });
 });
