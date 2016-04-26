4cbed161aecd2c0bea5d1f0f333643d944444c9b
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-04-26 18:36:54 -0400

    Reorder exports in utils/index.js

--- src/shared/actions/utils/index.js
+++ src/shared/actions/utils/index.js
@@ -1,10 +1,10 @@
 export { applyFlipRules } from './applyFlipRules/applyFlipRules';
 export { basicRule } from './basicRule/basicRule';
-export { getRandomHand } from './getRandomHand/getRandomHand';
 export { getCardToAdd } from './getCardToAdd/getCardToAdd';
 export { getCardToSelect } from './getCardToSelect/getCardToSelect';
 export { getNextPhase } from './getNextPhase/getNextPhase';
 export { getPieceToSelect } from './getPieceToSelect/getPieceToSelect';
+export { getRandomHand } from './getRandomHand/getRandomHand';
 export { getValidPiece } from './getValidPiece/getValidPiece';
 export { sameRule } from './sameRule/sameRule';
 export { sample } from './sample/sample';
