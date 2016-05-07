72501b7c0daaf8e122703f804b86b404e128092b
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-05-06 23:36:44 -0400

    #16 WIP make the deck a carousel.

--- src/shared/components/deck.jsx
+++ src/shared/components/deck.jsx
@@ -27,9 +27,48 @@ export class Deck extends React.Component {
             );
         });
 
+        let arrowWidth = 50;
+        let containerStyles = {
+            width: `calc(100% - ${arrowWidth * 2}px)`,
+            overflow: 'hidden',
+            display: 'inline-block'
+
+        };
+
+        let innerStyles = {
+            position: 'relative',
+            width: `${cards.length * 100}px`,
+            display: 'inline-block'
+        };
+
+        let arrowStyles = {
+            width: `${arrowWidth}px`,
+            display: 'inline-block',
+            verticalAlign: 'top'
+        };
+
+        let arrowIcon = {
+            lineHeight: '127px',
+            height: '127px',
+            textAlign: 'center',
+            width: '100%',
+            fontWeight: 200,
+            fontSize: '2em'
+        };
+
         return (
-            <div className="cards">
-            {cardsMarkup}
+            <div>
+                <div className="arrow" style={arrowStyles}>
+                    <i className="fa fa-angle-left" style={arrowIcon}></i>
+                </div>
+                <div className="cards" style={containerStyles}>
+                    <div style={innerStyles}>
+                        {cardsMarkup}
+                    </div>
+                </div>
+                <div className="arrow" style={arrowStyles}>
+                    <i className="fa fa-angle-right" style={arrowIcon}></i>
+                </div>
             </div>
         );
     }
