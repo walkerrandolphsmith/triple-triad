01d587de9df5886edf57aaf0cbdb8b109bda65a0
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-05-08 16:53:36 -0400

    Display placeholders for the candidate hand

--- src/shared/components/hand.jsx
+++ src/shared/components/hand.jsx
@@ -9,31 +9,40 @@ export class Hand extends React.Component {
 
     render() {
         let { score, cards, selectedCard, showBack} = this.props;
+        let hand = [];
+        let cardMarkup;
+        for(var i = 0; i < 5; i++){
+            const card = cards.get(i);
+            if(card) {
+                let name = showBack ? 'back' : card.get('name');
 
-        let cardsMarkup = cards.map((card, i) => {
+                const cardStyle = {
+                    cursor: card.get('owner') === 1 ? 'pointer' : 'default'
+                };
 
-            let name = showBack ? 'back' : card.get('name');
+                const classes = card.get('id') === selectedCard ? 'selected' : '';
 
-            const cardStyle = {
-                cursor: card.get('owner') === 1 ? 'pointer' : 'default'
-            };
-
-            const classes = card.get('id') === selectedCard ? 'selected' : '';
-
-            return (
-                <Card key={card.get('id')}
-                    card={card.set('name', name)}
-                    classes={classes}
-                    cardStyle={cardStyle}
-                    clickAction={this.click.bind(this, card.get('id'))}
-                />
+                cardMarkup = (
+                    <Card key={card.get('id')}
+                          card={card.set('name', name)}
+                          classes={classes}
+                          cardStyle={cardStyle}
+                          clickAction={this.click.bind(this, card.get('id'))}
+                    />
+                );
+            }else {
+                cardMarkup = (<div></div>);
+            }
+            hand.push(
+                <div className="card-placeholder">
+                    {cardMarkup}
+                </div>
             );
-        });
-
+        }
         return (
             <div className="hand">
                 <div className="score">{score}</div>
-                {cardsMarkup}
+                {hand}
             </div>
         );
     }
