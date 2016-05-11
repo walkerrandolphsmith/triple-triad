3a4a190f4340b1895c348d57ce1073ae6bd214a2
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-05-11 14:06:45 -0400

    dispatch getGame when firebase detects a change to the game.

--- src/shared/ducks/firebase/thunks/listenToGames.js
+++ src/shared/ducks/firebase/thunks/listenToGames.js
@@ -1,7 +1,16 @@
+import { getGameSuccess } from './../../game/actions/getGameSuccess';
+
 export const listenToGames = () => (dispatch, getState) => {
     const firebaseRef = getState().firebase.get('ref');
     firebaseRef.child('games').on('value', snapshot => {
         let games = snapshot.val();
 
+        Object.keys(games).forEach((key) => {
+            firebaseRef.child('games').child(key).on('value', snapshot => {
+                let game = snapshot.val();
+                game.id = key;
+                dispatch(getGameSuccess(game))
+            });
+        });
     });
 };
