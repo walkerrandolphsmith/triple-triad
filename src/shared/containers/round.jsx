ed6360dd9fd3a3ea1d16edacfc37c69bd6f10ce5
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-05-19 19:23:57 -0400

    15-disable to the a hand's card selection and board's piece selection when it is not your turn.

--- src/shared/containers/round.jsx
+++ src/shared/containers/round.jsx
@@ -3,14 +3,16 @@ import { bindActionCreators } from 'redux';
 import { Round } from './../components';
 import { boardSelector, handSelector, opponentHandSelector, scoreSelector, validPiecesSelector, currentGameSelector } from '../ducks/game';
 import { selectCard, completeTurn } from '../ducks/game';
+import { isCurrentPlayerMe } from './../utils/isCurrentPlayerMe';
 
 function mapStateToProps(state) {
     const { settings } = state;
     const game = currentGameSelector(state);
     let message = game.get('currentPlayerMessage');
-
+    let isMyTurn = isCurrentPlayerMe(game.get('currentPlayer'), state.auth.get('user').get('id'));
     return {
         game: currentGameSelector(state),
+        isMyTurn: isMyTurn,
         settings: settings,
         board: boardSelector(state),
         hand: handSelector(state),
