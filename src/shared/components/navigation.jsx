74f022d3e6cbd93b73c602a5b4bb64b297976a65
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-05-21 17:01:50 -0400

    Use react-router-bootstrap to create a responsive navbar.

--- src/shared/components/navigation.jsx
+++ src/shared/components/navigation.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router'
+import Navbar from 'react-bootstrap/lib/Navbar';
+import Nav from 'react-bootstrap/lib/Nav';
+import NavItem from 'react-bootstrap/lib/NavItem';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
+import NavDropdown from 'react-bootstrap/lib/NavDropdown';
+import LinkContainer from 'react-router-bootstrap/lib/LinkContainer';
 
 export class Navigation extends React.Component {
 
@@ -8,52 +14,51 @@ export class Navigation extends React.Component {
     }
 
     render() {
-        let { user, activeRoute, signOut } = this.props;
 
-        let homeLink = (
-            <li className={this.isActive(activeRoute, '/')}>
-                <Link to="/" className="navbar-brand"></Link>
-            </li>
-        );
+        let { user } = this.props;
 
         const navLinks = user ? ['games', 'leaderboard'] : [];
 
-        let links = navLinks.map((link, index) => {
-            const path = `/${link}`;
-            return (<li key={index} className={this.isActive(activeRoute, path)}>
-                <Link to={path}>{link}</Link>
-            </li>)
-        });
+        let links = navLinks.map((link, index) => (
+            <LinkContainer key={index} to={{ pathname: `/${link}` }}>
+                <NavItem eventKey={index}>
+                    <span>{link}</span>
+                </NavItem>
+            </LinkContainer>
+        ));
 
         let userLink = user
-            ? (<li className={this.isActive(activeRoute, '/user')}><Link to="/user"><img src="assets/images/default-user.png"/><span>{user}</span></Link></li>)
-            : (<li className={this.isActive(activeRoute, '/signin')}><Link to="/signin">SignIn</Link></li>);
+            ? (
+                <LinkContainer to={{ pathname: '/user' }}>
+                    <NavItem eventKey={1}>
+                        <img style={{width: '24px'}} src="assets/images/default-user.png"/>
+                        <span>{user}</span>
+                    </NavItem>
+                </LinkContainer>
+            )
+            : (
+                <LinkContainer to={{ pathname: '/signin' }}>
+                    <NavItem eventKey={1}><span>SignIn</span></NavItem>
+                </LinkContainer>
+            );
 
         return (
-            <nav className="navbar navbar-default" role="navigation">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        {homeLink}
-                    </div>
-
-                    <div className="collapse navbar-collapse" id="navbar-collapse">
-
-                        <ul className="container nav navbar-nav">
-                            {links}
-                        </ul>
-
-                        <ul className="nav navbar-nav navbar-right">
-                            {userLink}
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            <Navbar inverse>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to="/" eventKey={1}>Triple Triad</Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        {links}
+                    </Nav>
+                    <Nav pullRight>
+                        {userLink}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 }
