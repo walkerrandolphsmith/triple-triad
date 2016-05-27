34e6212117076b3f09470f8dab060714c2177dc8
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-05-26 21:40:34 -0400

    Formatting

--- src/shared/ducks/game/thunks/getGame.js
+++ src/shared/ducks/game/thunks/getGame.js
@@ -2,9 +2,12 @@ import { getGameRequest, getGameSuccess, getGameFailure } from './../index';
 
 export const getGame = id => (dispatch, getState) => {
     dispatch(getGameRequest());
-
-    const firebaseRef = getState().firebase.get('ref');
-    firebaseRef.child('games').child(id).on('value', snapshot => {
+    getState()
+        .firebase
+        .get('ref')
+        .child('games')
+        .child(id)
+        .once('value', snapshot => {
         let game = snapshot.val();
         game.id = id;
         dispatch(getGameSuccess(game));
