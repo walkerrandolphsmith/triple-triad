221b87a96cd2890f264d98e6bf5c07a5faea64e7
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-06-18 11:15:07 -0400

    Add newlines for readiability!

--- src/shared/utils/getFlips.js
+++ src/shared/utils/getFlips.js
@@ -4,5 +4,10 @@ import { sameRule } from './sameRule';
 export function getFlips(i, deck) {
     const basic = basicRule(i, deck);
     const same = sameRule(i, deck);
-    return basic.concat(same).reduce((indexes, index) => indexes.findIndex(j => j === index) < 0 ? indexes.concat(index) : indexes, []);
+    return basic
+        .concat(same)
+        .reduce(
+            (indexes, index) => indexes.findIndex(j => j === index) < 0 ? indexes.concat(index) : indexes
+            , []
+        );
 }
